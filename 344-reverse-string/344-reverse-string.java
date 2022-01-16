af234500633b0f823a28408eca4c53a6cffcb213
brwnsugr <eunho@tumblbug.com>
2022-01-16 16:33:27 +0900

    Time: 2 ms (27.70%), Space: 51.1 MB (40.65%) - LeetHub

--- 344-reverse-string/344-reverse-string.java
+++ 344-reverse-string/344-reverse-string.java
@@ -1,8 +1,7 @@
 class Solution {
     public void reverseString(char[] s) {
         int len = s.length;
-        for(int i = 0; i <= len/2; i++) {
-            if(i == len/2 && len % 2 == 0) break;
+        for(int i = 0; i < len/2; i++) {
             int complement = len - i - 1;
             char temp = s[i];
             s[i] = s[complement];
