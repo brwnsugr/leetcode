84a7963da929c6e56d493fcc09a2c4d232bc1078
brwnsugr <eunho@tumblbug.com>
2022-01-20 12:34:01 +0900

    Time: 3 ms (10.82%), Space: 40.4 MB (7.55%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.java
+++ 206-reverse-linked-list/206-reverse-linked-list.java
@@ -9,31 +9,24 @@
  * }
  */
 class Solution {
-  public ListNode reverseList(ListNode head) {
-    /**
-     * ListNode 그대로 순회 -> 새로운 list 에다가 저장함 (ArrayList) -> listNode 의 길이를 모르기 때문에 동적으로 element를 넣을 수 있는 구조로 가는게 좋음
-     * 
-     * list node :  1->3-5,  new arraylist : 1,3,5  -> iterate the new arrayslist in reverse (5->3->1) 
-     * 
-     */
-    if(head == null || head.next == null) return head;
-    List<Integer> arr = new ArrayList<>();
-    
-    while(head != null) {
-      arr.add(head.val);
-      head = head.next;
-    }
-    
-    //traverse the arr in reverse and save to the new ListNode simultaneously.
+    public ListNode reverseList(ListNode head) {
+        //list 
+        
+        if(head == null) return head;
+        List<Integer> arr = new ArrayList<>();
+        while(head != null) {
+            arr.add(head.val);
+            head = head.next;
+        }
+        
+        ListNode dummy = new ListNode();
+        ListNode curr = dummy;
 
-    ListNode dummy = new ListNode();
-    ListNode curr = dummy;
-    
-    for(int i = arr.size()-1; i >= 0; i--) {
-      curr.next = new ListNode(arr.get(i));
-      curr = curr.next;
+        for(int i = arr.size()-1; i >= 0; i--) {
+          curr.next = new ListNode(arr.get(i));
+          curr = curr.next;
+        }
+
+        return dummy.next;
     }
-    
-    return dummy.next;
-  }
 }
