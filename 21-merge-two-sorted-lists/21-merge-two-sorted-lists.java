b928c5f62aef07e8bf60080f6e073e2c49c4f3b4
brwnsugr <eunho@tumblbug.com>
2022-01-20 12:58:57 +0900

    Time: 2 ms (6.55%), Space: 40.1 MB (11.54%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
@@ -9,43 +9,32 @@
  * }
  */
 class Solution {
-  public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-    /**
-     * First, make new Linked list to save the merged list of list1, list2
-     * In short, it's a linked list to save the concatenated the two lists.
-     *
-     * iterate list1 list2 at the same time, and pick both element from that two lists
-     * and comapare these element and just put the smaller one to the new linked list,
-     * and remove the smaller element from those linked lists.
-     * It's kind of recursive approach
-     *
-     * just compare and compare when is the end of that recursive computing?
-     * if we encounter both two linkedLists are all null value. (both two lists don't have any valid nodes)
-     *
-     *
-     */
-
-    ListNode dummy = new ListNode(-1);
-
-    ListNode curr = dummy;
-    recursive(curr, list1, list2);
-    return dummy.next;
-  }
-
-  private void recursive(ListNode curr, ListNode list1, ListNode list2) {
-    if(list1 == null && list2 == null) return;
-    else{
-      int val1 = list1 == null ? 101 : list1.val;
-      int val2 = list2 == null ? 101 : list2.val;
-      if(val1 < val2) {
-        curr.next = list1;
-        list1 = list1.next;
-      }
-      else {
-        curr.next = list2;
-        list2 = list2.next;
-      }
-      recursive(curr.next, list1, list2);
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+       ListNode dummy = new ListNode(-1);
+       ListNode curr = dummy;
+        
+       recursive(curr, list1, list2);
+        
+       return dummy.next;
     }
-  }
+    
+    private void recursive(ListNode curr, ListNode list1, ListNode list2) {
+        while(list1 != null && list2 != null) {
+            int val1 = list1 == null ? 101 : list1.val;
+            int val2 = list2 == null ? 101 : list2.val;
+            if(val1 <= val2) {
+                curr.next = list1;
+                list1 = list1.next;
+            }
+            else {
+                curr.next = list2;
+                list2 = list2.next;
+            }
+            curr = curr.next;
+        }
+        if(list1 != null) curr.next = list1;
+        if(list2 != null) curr.next = list2;
+        
+    }
+    
 }
