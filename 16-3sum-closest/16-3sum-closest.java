f16cfe2c495458d4e7d779948c437d396e5b0277
brwnsugr <eunho@tumblbug.com>
2022-01-31 23:37:26 +0900

    Time: 6 ms (72.08%), Space: 41.8 MB (5.07%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.java
+++ 16-3sum-closest/16-3sum-closest.java
@@ -1,43 +1,28 @@
 class Solution {
-  private int diff;
   public int threeSumClosest(int[] nums, int target) {
-
-    /**
-     * My Idea is that when going through nums array, we pick one element and set the complement the element that we're at subtracted from the target
-     * I mean the complement = target - nums[i] ;
-     *
-     * So, in the right remain elements of the nums array , we
-     *
-     *
-     * go through nums[i],
-     * p
-     *
-     * comp = target - nums[i]
-     */
+    int minDiff = Integer.MAX_VALUE;
     int ans = 0;
-    diff = Integer.MAX_VALUE;
-
-    for(int i = 0; i < nums.length-2; i++) {
+    Arrays.sort(nums);
+    for(int i = 0; i < nums.length -2; i++) {
       int firstOne = nums[i];
-      int complement = target - firstOne;
+      int semiTarget = target - firstOne;
 
-      //firstOne 의 오른쪽에 있는 두개의 인덱스의 끝값을 인자로 넣어서 하나씩 탐색합니다.
-      int remainTwoSum = getClosestSum(nums, i+1, nums.length, complement);
-      if(remainTwoSum != 2001) ans = firstOne + remainTwoSum;
-    }
-    return ans;
-  }
-
-  private int getClosestSum(int[] nums, int start, int end, int semiTarget) {
-    int ret = 2001;
-    for(int i = start; i < end-1; i++) {
-      for(int j = i+1; j < end; j++) {
-        if (Math.abs(semiTarget - (nums[i] + nums[j])) < diff) {
-          diff = Math.abs(semiTarget - (nums[i] + nums[j]));
-          ret = nums[i] + nums[j];
+      int left = i+1;
+      int right = nums.length - 1;
+      while(left < right) {
+        int diff = Math.abs(semiTarget - (nums[left] + nums[right]));
+        if(diff < minDiff) {
+          minDiff = diff;
+          ans = firstOne + nums[left] + nums[right];
+        }
+        if(semiTarget > nums[left] + nums[right]) {
+          left++;
+        }
+        else {
+          right--;
         }
       }
     }
-    return ret;
+    return ans;
   }
 }
