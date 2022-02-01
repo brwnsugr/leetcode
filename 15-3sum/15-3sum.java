0046be47efd6e0ceed672c648bbaf94898026b53
brwnsugr <eunho@tumblbug.com>
2022-02-01 12:08:00 +0900

    Time: 314 ms (25.41%), Space: 146.7 MB (5.43%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,32 +1,26 @@
 class Solution {
-  List<List<Integer>> res = new ArrayList<>();
+  private List<List<Integer>> res = new ArrayList<>();
 
   public List<List<Integer>> threeSum(int[] nums) {
-    int target = 0;
     Arrays.sort(nums);
-    for(int i = 0; i< nums.length-2; i++) {
-      int semiTarget = target - nums[i];
-      /**
-       * we get Two Sum from the two indices which are bigger than ith idx
-       */
+    for(int i = 0; i < nums.length; i++) {
+      if(nums[i] > 0) break;
       if(i == 0 || nums[i-1] != nums[i]) {
-        getTwoSum(nums, i+1, nums.length, semiTarget);
+        addPossibleSum(nums, i);
       }
     }
     return res;
   }
 
-  private void getTwoSum(int[] nums, int start, int end, int target) {
+  private void addPossibleSum(int[] nums, int i) {
     Set<Integer> s = new HashSet<>();
-    for(int i = start; i < end; i++) {
-      int complement = target - nums[i];
+    for(int j = i+1; j < nums.length; j++) {
+      int complement = -nums[j] - nums[i];
       if(s.contains(complement)) {
-        res.add( Arrays.asList(-target, complement, nums[i]));
-        while(i + 1 < end && nums[i] == nums[i+1]) {
-          i++;
-        }
+        res.add(Arrays.asList(nums[i], complement, nums[j]));
+        while(j + 1 < nums.length && nums[j] == nums[j+1]) j++;
       }
-      s.add(nums[i]);
+      s.add(nums[j]);
     }
   }
 }
