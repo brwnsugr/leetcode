b1cc4617303edcdc9a186840cbffd3a4fdb41e64
brwnsugr <eunho@tumblbug.com>
2022-02-01 13:39:42 +0900

    Time: 581 ms (15.95%), Space: 120.2 MB (10.66%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,31 +1,22 @@
 class Solution {
-  private List<List<Integer>> res = new ArrayList<>();
-
   public List<List<Integer>> threeSum(int[] nums) {
-    Arrays.sort(nums);
-    for(int i = 0; i < nums.length -2; i++) {
-      if(nums[i] > 0) break;
-      if(i == 0 || nums[i-1] != nums[i]) addThreeSum(nums, i);
-    }
-    return res;
-  }
-
-  private void addThreeSum(int[] nums, int i) {
-    int lo = i + 1;
-    int hi = nums.length-1;
-    while(lo < hi) {
-      int sum = nums[lo] + nums[hi] + nums[i];
-      if(sum < 0) {
-        lo++;
-      }
-      else if(sum > 0) {
-        hi--;
-      }
-      else {
-        res.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
-        lo++; hi--;
-        while(lo < hi && nums[lo] == nums[lo-1]) lo++;
+    Set<List<Integer>> resSet = new HashSet<>();
+    Set<Integer> firstDups = new HashSet<>();
+    Map<Integer, Integer> seen = new HashMap<>();
+    
+    for(int i = 0; i < nums.length - 2; i++) {
+      if(firstDups.add(nums[i])) {
+        for(int j = i+1; j < nums.length; j++) {
+          int complement = -nums[i] -nums[j];
+          if(seen.containsKey(complement) && seen.get(complement) == i) {
+            List<Integer> res = Arrays.asList(complement, nums[i], nums[j]);
+            Collections.sort(res);
+            resSet.add(res);
+          }
+          seen.put(nums[j], i);
+        }
       }
     }
+    return new ArrayList<>(resSet);
   }
 }
