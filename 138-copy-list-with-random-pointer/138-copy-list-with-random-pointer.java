9990b0473b024660bddc3d24e56fd95222a59453
brwnsugr <eunho@tumblbug.com>
2022-02-02 19:01:32 +0900

    Time: 0 ms (100.00%), Space: 41.8 MB (55.97%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.java
@@ -14,60 +14,18 @@ class Node {
 */
 
 class Solution {
+  private Map<Node, Node> visitedNodeMap = new HashMap<>();
   public Node copyRandomList(Node head) {
     if(head == null) return null;
-    Node dummy = new Node(0);
-    Node copyCurrNode = dummy;
 
-    Node currNode = head;
-    int count = 0;
-    while(currNode != null) {
-      count++;
-      Node nodeToAdd = new Node(currNode.val);
-      copyCurrNode.next = nodeToAdd;
-      copyCurrNode = copyCurrNode.next;
-      currNode = currNode.next;
-    }
+    if(visitedNodeMap.containsKey(head)) return visitedNodeMap.get(head);
 
-    currNode = head;
-    List<Integer> randomIdxList = new ArrayList<>();
-    while(currNode != null) {
-      if(currNode.random == null) randomIdxList.add(null);
-      else randomIdxList.add(count - distToEnd(currNode.random) - 1);
-      currNode = currNode.next;
-    }
+    Node newNode = new Node(head.val);
+    visitedNodeMap.put(head, newNode);
 
+    newNode.next = this.copyRandomList(head.next);
+    newNode.random = this.copyRandomList(head.random);
 
-    int nodeIdx = 0;
-      
-    currNode = dummy.next;
-
-    while(currNode != null) {
-      if(randomIdxList.get(nodeIdx) == null) currNode.random = null;
-      if(randomIdxList.get(nodeIdx) != null) currNode.random = getNthNode(dummy.next, randomIdxList.get(nodeIdx));
-
-      currNode = currNode.next;
-      nodeIdx++;
-    }
-    return dummy.next;
-  }
-
-  public Node getNthNode(Node head, int idx) {
-    Node curr = head;
-    while(idx > 0) {
-      curr = curr.next;
-      idx--;
-    }
-    return curr;
-  }
-
-  public int distToEnd(Node currNode) {
-    int distToEnd = 0;
-    Node startNode = currNode;
-    while(startNode.next != null) {
-      distToEnd++;
-      startNode = startNode.next;
-    }
-    return distToEnd;
+    return newNode;
   }
 }
