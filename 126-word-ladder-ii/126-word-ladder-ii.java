dddf1d5ca61acc97e7404e10c557e78b9ef1ccd2
brwnsugr <eunho@tumblbug.com>
2022-02-11 23:55:18 +0900

    Time: 135 ms (5.01%), Space: 117.4 MB (5.04%) - LeetHub

--- 126-word-ladder-ii/126-word-ladder-ii.java
+++ 126-word-ladder-ii/126-word-ladder-ii.java
@@ -1,72 +1,95 @@
 class Solution {
-  List<List<String>> answer = new ArrayList<>();
-//  int minLen = Integer.MAX_VALUE;
-
   public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
+    /**
+     * Naive Approach
+     * from the start word, we traverse the next word that differs by a single letter
+     * and do it recursively while we wouldn't get to the next anymore.
+     * and we if we could visit all, put into this words in order as one of the answers list.
+     *
+     * Implementation Idea
+     * 'dog' -> 'dot', 'hog'
+     * 'dot' -> 'hot', 'bot'
+     * Map key: word,  value: List of Adjacent Words of word as key
+     *
+     * 1. building up adjListMap
+     * 2. Traversing it By short distance words first  a->[b,c] -> d
+     * a -> b-> c (x) a->b
+     *                   c->d
+     * & mark each as visited.
+     *
+     * TC: build adjMap -> O(N^2) (try to generate all the pairs of it)
+     * traverse it : O(N*N) SC: O(N*N)
+     */
 
-    if(wordList.size() == 1) {
-      if(endWord.equals(wordList.get(0))) {
-        answer.add(Arrays.asList(endWord));
-        return answer;
-      }
-      return answer;
-    }
-    if(!wordList.contains(beginWord)) wordList.add(beginWord);
+    wordList.add(beginWord);
     Map<String, List<String>> adjMap = new HashMap<>();
-    int wordLen = wordList.get(0).length();
+    for(String word : wordList) adjMap.put(word, new ArrayList<>());
 
-    for(int i = 0; i < wordList.size(); i++) {
-      for(int j = 0; j < wordLen; j++) {
-        String proto = makeProto(wordList.get(i), j);
-        if(!adjMap.containsKey(proto)) adjMap.put(proto, new ArrayList<>());
-        adjMap.get(proto).add(wordList.get(i));
-      }
-    }
+    /**
+     * build up adj wordlist in this map value
+     */
 
-//    Set<String> seen = new HashSet<>();
-//    seen.add(beginWord);
-//    List<String> path = new ArrayList<>();
-//    path.add(beginWord);
-    bfs(adjMap, beginWord, endWord);
-    return answer;
-  }
-  
-  private void bfs(Map<String, List<String>> adjMap, String beginWord, String target) {
-    Set<String> visited = new HashSet<>();
-    Queue<List<String>> q = new LinkedList<>();
-    
-    List<String> path = new ArrayList<>();
-    
-    path.add(beginWord);
-    boolean found = false;
-    q.add(path);
-    
-    while(!q.isEmpty() && !found) {
-      int count = q.size();
-      for(int i = 0; i < count; i++) {
-        List<String> currPath = q.poll();
-        if(currPath.get(currPath.size() -1).equals(target)) {
-          found = true;
-          answer.add(new ArrayList<>(currPath));
-        }
-        String currWord = currPath.get(currPath.size()-1);
-        visited.add(currWord);
-        for(int j = 0; j < currWord.length(); j++) {
-          String proto = makeProto(currWord, j);
-          List<String> nextWords = adjMap.get(proto);
-          for(String nextWord : nextWords) {
-            if(!visited.contains(nextWord)) {
-              currPath.add(nextWord);
-              q.add(new ArrayList<>(currPath));
-              currPath.remove(nextWord);
-            }
+    for(int i = 0; i < wordList.size()-1; i++) {
+      for(int j = i+1; j < wordList.size(); j++) {
+        String firstWord = wordList.get(i);
+        String secondWord = wordList.get(j);
+        for(int k = 0; k < firstWord.length(); k++) {
+          if(buildOneBlurredWord(firstWord, k)
+              .equals(buildOneBlurredWord(secondWord, k))){
+            adjMap.get(firstWord).add(secondWord);
+            adjMap.get(secondWord).add(firstWord);
+            break;
           }
         }
       }
     }
+
+    /**
+     * BFS
+     */
+
+
+    Set<List<String>> answerSet = new HashSet<>();
+    Queue<List<String>> q = new LinkedList<>();
+
+    List<String> curr = new ArrayList<>();
+    curr.add(beginWord);
+    q.add(curr);
+    Set<String> visited = new HashSet<>();
+
+    boolean foundEndWord = false;
+    while(!q.isEmpty()) {
+      int qSize = q.size();
+      for(int i = 0; i < qSize; i++) {
+        List<String> currList = q.poll();
+        String lastWord = currList.get(currList.size()-1);
+        visited.add(lastWord);
+        if(lastWord.equals(endWord)) {
+          answerSet.add(currList);
+          foundEndWord = true;
+        }
+        List<String> adjWords = adjMap.get(lastWord);
+        for(String adjWord: adjWords) {
+          if(!visited.contains(adjWord)) {
+            currList.add(adjWord);
+            q.add(new ArrayList<>(currList));
+            currList.remove(adjWord);
+          }
+        }
+      }
+      if(foundEndWord) break;
+    }
+
+    List<List<String>> answerList = new ArrayList<>();
+    for(List<String> path : answerSet) {
+      answerList.add(path);
+    }
+    return answerList;
   }
 
-  public String makeProto(String word, int idx){
-    return word.substring(0,idx) + "*" + word.substring(idx+1);
+  private String buildOneBlurredWord(String input, int pos) {
+    char[] chars = input.toCharArray();
+    chars[pos] = '*';
+    return new String(chars);
   }
 }
