bfacd122e288627aefa5059e4cce3f64c4883936
brwnsugr <eunho@tumblbug.com>
2022-02-18 01:18:32 +0900

    Time: 131 ms (20.18%), Space: 71.1 MB (31.31%) - LeetHub

--- 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.java
+++ 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.java
@@ -11,20 +11,13 @@ class Solution {
         Stack<Integer> s = new Stack<>();
         for(int i = 0; i< len; i++) { // s = [1,2], left = [1, 2, 1, 1]
             while(!s.isEmpty() && arr[s.peek()] > arr[i]) {
+                right[s.peek()] = i - s.peek();
                 s.pop();
             }
             left[i] = s.isEmpty() ? i + 1 : (i - s.peek()); 
             s.push(i);
         }
-        
-        s = new Stack<>();
-        for(int i = 0; i< len; i++) { // s = [1,2], left = [1, 2, 1, 1]
-            while(!s.isEmpty() && arr[s.peek()] > arr[i]) {
-                right[s.peek()] = i - s.peek();
-                s.pop();
-            }
-            s.push(i);
-        }
+    
         
         long ans = 0;
         long mod = (int)1e9 + 7;
