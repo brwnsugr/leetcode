1be29254022ce20e4f3261c78d8fe6c520348778
brwnsugr <eunho@tumblbug.com>
2022-02-19 20:38:07 +0900

    Time: 337 ms (10.54%), Space: 209.7 MB (5.02%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.java
+++ 1268-search-suggestions-system/1268-search-suggestions-system.java
@@ -1,24 +1,47 @@
 class Solution {
-    public List<List<String>> suggestedProducts(String[] products, String searchWord) {
-        /*
-        1. Create a trie from the given products input
-        2. 
+/*
+Using Trie.
+N = length of products array, M = length of searchWord, K = length of longest product in products
+TC: O(N*K) + O(M*K)
+SC: O(K)
+
+using trie
+we store the each product of the product array to the tries and 
+every prefix searchkeyword we trying to find 
+
+n = length pr arr, m = len of searchword =, k = length of longest lenghth product
+
+TC: O(k * n) + O(m * k)
+SC: O(k)
+
+TC: O(nlogn + m*logn)
+SC: O()
+*/
     
-        */
+    
+    
+    
+    public List<List<String>> suggestedProducts(String[] products, String searchWord) {
+        
         Trie trie = new Trie();
+        List<List<String>> answer = new ArrayList<>();
         
         for(String product : products) {
             trie.insert(product);
         }
         
         String prefix = "";
-        List<List<String>> answerList = new ArrayList<>();
         
         for(char c : searchWord.toCharArray()) {
             prefix += c;
-            answerList.add(trie.getWordStartingWith(prefix));
+            List<String> temp = trie.getWordStartingWith(prefix);
+            answer.add(temp);
         }
-        return answerList;
+        
+        
+        return answer;
+        
+
     }
     
     
@@ -30,46 +53,51 @@ class Trie {
         List<Node> children = Arrays.asList(new Node[26]);
     }
     
-    Node root, curr;
+    Node root; 
+    Node curr;
     List<String> resultBuffer;
     
     void dfsWithPrefix(Node curr, String word) {
-        if(resultBuffer.size() == 3) return;
+        if(resultBuffer.size()==3) return;
         if(curr.isWord) resultBuffer.add(word);
         
+        //Run DFS on all possible path
         for(char c = 'a'; c <= 'z'; c++) {
             if(curr.children.get(c-'a') != null) {
+                
                 dfsWithPrefix(curr.children.get(c-'a'), word + c);
             }
         }
+        return;
     }
     
     Trie() {
         root = new Node();
-        
     }
     
     //Insert the String into Trie
     void insert(String s) {
         curr = root;
-        
         for(char c : s.toCharArray()) {
+            // Node next = curr.children.get(c-'a');
             if(curr.children.get(c-'a') == null) {
                 curr.children.set(c-'a', new Node());
             }
             curr = curr.children.get(c-'a');
         }
-        
-        curr.isWord = true; // Mark that complete word as isWord true
+        curr.isWord = true;
     }
     
     List<String> getWordStartingWith(String prefix) {
-        curr = root; // Starting from the root
+        Node curr = root;
         resultBuffer = new ArrayList<>();
-        
         for(char c : prefix.toCharArray()) {
-            if(curr.children.get(c-'a') == null) return resultBuffer;
-            curr = curr.children.get(c-'a');
+            if(curr.children.get(c-'a') != null) {
+                curr = curr.children.get(c-'a');
+            }
+            else {
+                return resultBuffer;
+            }
         }
         dfsWithPrefix(curr, prefix);
         return resultBuffer;
