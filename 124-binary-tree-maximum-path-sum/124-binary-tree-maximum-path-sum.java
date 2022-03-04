7007a8531c57e2b77889fa609305f431423b9e69
brwnsugr <eunho@tumblbug.com>
2022-03-04 20:57:46 +0900

    Time: 1 ms (81.59%), Space: 48 MB (21.02%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.java
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.java
@@ -14,21 +14,22 @@
  * }
  */
 class Solution {
-  private int answer;
-
-  public int maxPathSum(TreeNode root) {
-    if(root.left == null && root.right == null) return root.val;
-    answer = Integer.MIN_VALUE;
-    getMaxSum(root);
-    return answer;
-  }
-
-  private int getMaxSum(TreeNode currNode) {
-    if(currNode == null) return 0;
-    int leftMax = Math.max(getMaxSum(currNode.left), 0);
-    int rightMax = Math.max(getMaxSum(currNode.right), 0);
-    int newPathSum = currNode.val + leftMax + rightMax;
-    answer = Math.max(newPathSum, answer);
-    return currNode.val + Math.max(leftMax, rightMax);
-  }
+    
+    int answer = Integer.MIN_VALUE;
+    
+    public int maxPathSum(TreeNode root) {
+        recursive(root);
+        return answer;
+    }
+    
+    private int recursive(TreeNode curr) {
+        if(curr == null) return 0;
+        
+        int leftGain = Math.max(recursive(curr.left), 0);
+        int rightGain = Math.max(recursive(curr.right), 0);
+    
+        int newPathSum = curr.val + leftGain + rightGain;
+        answer = Math.max(newPathSum, answer);    
+        return curr.val + Math.max(leftGain, rightGain);
+    }
 }
