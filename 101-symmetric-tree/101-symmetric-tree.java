2a182459f122f705566bd555cb6f2e9fa6670184
brwnsugr <eunho@tumblbug.com>
2022-03-09 02:23:22 +0900

    Time: 0 ms (100.00%), Space: 40 MB (61.22%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.java
+++ 101-symmetric-tree/101-symmetric-tree.java
@@ -25,26 +25,16 @@ class Solution {
     private List<Integer> elementsByLevel = new ArrayList<>();
     
     public boolean isSymmetric(TreeNode root) {
-        int leftVal = root.left == null ? -101 : root.left.val;
-        int rightVal = root.right == null ? -101 : root.right.val;
-        if(leftVal != rightVal) return false;
         return recursive(root.left, root.right);
     }
     
     private boolean recursive(TreeNode left, TreeNode right) {
         if(left == null && right == null) return true;
         if(left == null || right == null) return false;
-        int leftLeftVal = left.left == null ? -101 : left.left.val;
-        int rightRightVal = right.right == null ? -101 : right.right.val;
-        
-        int leftRightVal = left.right == null ? -101 : left.right.val;
-        int rightLeftVal = right.left == null ? -101 : right.left.val;
-        
-        if(leftLeftVal == rightRightVal 
-               && leftRightVal == rightLeftVal 
-               && recursive(left.left, right.right)
-               && recursive(left.right, right.left)) { 
-                return true;
+        if(left.val == right.val 
+           && recursive(left.left, right.right) 
+           && recursive(left.right, right.left)) { 
+            return true;
         }
         return false;
         
