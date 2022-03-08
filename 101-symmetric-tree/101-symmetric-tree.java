b070c2cfa80b028d31286059165491fcb2ee7dd4
brwnsugr <eunho@tumblbug.com>
2022-03-09 02:18:09 +0900

    Time: 1 ms (59.58%), Space: 42 MB (43.90%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.java
+++ 101-symmetric-tree/101-symmetric-tree.java
@@ -12,19 +12,43 @@
  *         this.right = right;
  *     }
  * }
+ 
+    1^
+    |\
+   2   2
+  / \  /\
+ 3. 4. 4.3
+    
  */
 class Solution {
-  public boolean isSymmetric(TreeNode root) {
-    if(root==null) return true;
-    return isValid(root.left, root.right);
-  }
-
-  private boolean isValid(TreeNode left, TreeNode right) {
-    if(left == null && right == null) return  true;
-    else if(left== null || right == null) return false;
-    else if(left.val == right.val) {
-      return isValid(left.left, right.right) && isValid(left.right, right.left);
+    
+    private List<Integer> elementsByLevel = new ArrayList<>();
+    
+    public boolean isSymmetric(TreeNode root) {
+        int leftVal = root.left == null ? -101 : root.left.val;
+        int rightVal = root.right == null ? -101 : root.right.val;
+        if(leftVal != rightVal) return false;
+        return recursive(root.left, root.right);
     }
-    else return false;
-  }
+    
+    private boolean recursive(TreeNode left, TreeNode right) {
+        if(left == null && right == null) return true;
+        if(left == null || right == null) return false;
+        int leftLeftVal = left.left == null ? -101 : left.left.val;
+        int rightRightVal = right.right == null ? -101 : right.right.val;
+        
+        int leftRightVal = left.right == null ? -101 : left.right.val;
+        int rightLeftVal = right.left == null ? -101 : right.left.val;
+        
+        if(leftLeftVal == rightRightVal 
+               && leftRightVal == rightLeftVal 
+               && recursive(left.left, right.right)
+               && recursive(left.right, right.left)) { 
+                return true;
+        }
+        return false;
+        
+    }
+    
+    
 }
