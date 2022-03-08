d7c7038126621adbd2f7a552744a88e087d7f311
brwnsugr <eunho@tumblbug.com>
2022-03-09 08:07:06 +0900

    Time: 43 ms (27.21%), Space: 41.8 MB (30.22%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
@@ -1,24 +1,18 @@
 class Solution {
     public int findTheWinner(int n, int k) {
-        
-        /** k=2               
-        queue q = [3];  qSize = 5
-        
-        */
         Queue<Integer> q = new LinkedList<>();
         
         //enqueue all the elements
-        
         for(int i = 1; i <= n; i++) q.add(i);
+        
         int counter = 0;
         while(q.size() != 1) {
             int curr = q.poll();
             counter++;
-            if(counter == k) {
+            if(counter == k) { // when counter meets k'th, we reset counter and would not enqueue this elements.
                 counter = 0;
-                continue;
             }
-            q.add(curr);
+            else q.add(curr);
         }
         
         int ret = q.poll();
