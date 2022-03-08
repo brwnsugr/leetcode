fa857a6cdd2071b4ed8886886139b06c6b931fbb
brwnsugr <eunho@tumblbug.com>
2022-03-09 07:25:55 +0900

    Time: 2 ms (12.53%), Space: 43.8 MB (5.12%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.java
+++ 101-symmetric-tree/101-symmetric-tree.java
@@ -12,30 +12,31 @@
  *         this.right = right;
  *     }
  * }
- 
-    1^
-    |\
-   2   2
-  / \  /\
- 3. 4. 4.3
-    
  */
 class Solution {
-
+    
+    List<TreeNode> elementsByLevel = new ArrayList<>();
+    
     public boolean isSymmetric(TreeNode root) {
-        return recursive(root.left, root.right);
-    }
-    
-    private boolean recursive(TreeNode left, TreeNode right) {
-        if(left == null && right == null) return true;
-        else if(left == null || right == null) return false;
-        else if(left.val == right.val 
-           && recursive(left.left, right.right) 
-           && recursive(left.right, right.left)) { 
-            return true;
+        
+        Queue<TreeNode> q = new LinkedList<>();
+
+        q.add(root);
+        q.add(root);
+        
+        while(!q.isEmpty()) {
+            TreeNode t1 = q.poll();
+            TreeNode t2 = q.poll();
+            if(t1 == null && t2 == null) continue;
+            if(t1 == null || t2 == null) return false;
+            if(t1.val != t2.val) return false;
+            
+            q.add(t1.left);
+            q.add(t2.right);
+            q.add(t1.right);
+            q.add(t2.left);
         }
-        else return false;
+        
+        return true;
     }
-    
-    
 }
