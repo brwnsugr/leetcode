abd5ddbb9201c87f022cbce91b7ceb615ba32b1d
brwnsugr <eunho@tumblbug.com>
2022-03-12 11:23:08 +0900

    Time: 76 ms (36.87%), Space: 126 MB (5.01%) - LeetHub

---
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -0,0 +1,37 @@
+class Solution {
+    
+    private int target;
+    private Boolean cache[][];
+    
+    public boolean canPartition(int[] nums) {
+        
+        target = 0;
+        int sum = 0;
+        for(int num : nums) sum += num;
+        if(sum % 2 == 1) return false;
+        target = sum / 2;
+        cache = new Boolean[nums.length+1][sum+1];
+        
+        return isValid(nums, nums.length, 0);
+    }
+    
+    private boolean isValid(int[] nums, int remainCount, int cumSum) {
+        if(cumSum == target) {
+            cache[remainCount][cumSum] = true;
+            return true;
+        }
+        if(remainCount == 0 || cumSum > target) {
+            cache[remainCount][cumSum] = false;
+            return false;
+        }
+        if(cache[remainCount][cumSum] != null) return cache[remainCount][cumSum]; 
+        if(isValid(nums, remainCount-1, cumSum+nums[remainCount-1]) || isValid(nums, remainCount-1, cumSum)) {
+            cache[remainCount][cumSum] = true;
+            return true;
+        }
+        
+        cache[remainCount][cumSum] = false;
+        return false;
+        
+    }
+}
