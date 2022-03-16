1a6a2850e61201d7f42901ca364445bd3b00fb82
brwnsugr <eunho@tumblbug.com>
2022-03-17 01:11:57 +0900

    Time: 0 ms (100.00%), Space: 41.9 MB (50.32%) - LeetHub

--- 59-spiral-matrix-ii/59-spiral-matrix-ii.java
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.java
@@ -9,33 +9,30 @@ class Solution {
         int right = n-1;
         
         int count = 0;
-        while(true) {
+        while(count != totalCount) {
             //right
             for(int col = left; col <= right; col++) {
                 board[up][col] = ++count;
             }
             up++;
-            if(count >= totalCount) break;
+            
             //down
             for(int row = up; row <= down; row++) {
                 board[row][right] = ++count;
             }
             right--;
-            if(count >= totalCount) break;
             
             //left
             for(int col = right; col >= left; col--) {
                 board[down][col] = ++count;
             }
             down--;
-            if(count >= totalCount) break;
             
             //up
             for(int row = down; row >= up; row--) {
                 board[row][left] = ++count;
             }
             left++;
-            if(count >= totalCount) break;
         }
         
         return board;
