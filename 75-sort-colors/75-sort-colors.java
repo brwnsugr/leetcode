5ed3dd7996a0405d354cbf3b24b57a90c97789b9
brwnsugr <eunho@tumblbug.com>
2022-03-26 15:22:00 +0900

    Time: 0 ms (100.00%), Space: 40.6 MB (84.86%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -2,36 +2,25 @@ class Solution {
     public void sortColors(int[] nums) {
         // Naive Approach is sort 
         
-        // nums
+        int p0 = 0;
+        int curr = 0;
         
-        // nums1 = [2,0,2,1,1,0];
-        
-        Map<Integer, Integer> visitCount = new HashMap<>();
-        
-        for(int num : nums) {
-            if(!visitCount.containsKey(num)){
-                visitCount.put(num, 1);
+        int p2 = nums.length - 1;
+
+        while(curr <= p2) {
+            if(nums[curr] == 0) {
+                int tmp = nums[p0];
+                nums[p0++] = 0;
+                nums[curr++] = tmp;
+            }
+            else if(nums[curr] == 2) {
+                int tmp = nums[p2];
+                nums[p2--] = nums[curr];
+                nums[curr] = tmp;
             }
             else {
-                visitCount.put(num, visitCount.get(num) + 1);
+                curr++;
             }
         }
-        
-        for(int i = 0; i < nums.length; i++) {
-            if(visitCount.containsKey(0) && visitCount.get(0) > 0) {
-                nums[i] = 0;
-                visitCount.put(0, visitCount.get(0) - 1);
-            }
-            else if(visitCount.containsKey(1) && visitCount.get(1) > 0) {
-                nums[i] = 1;
-                visitCount.put(1, visitCount.get(1) - 1);
-            }
-            else if(visitCount.containsKey(2) && visitCount.get(2) > 0) {
-                nums[i] = 2;
-                visitCount.put(2, visitCount.get(2) - 1);
-            }
-        }
-        return;
-        
     }
 }
