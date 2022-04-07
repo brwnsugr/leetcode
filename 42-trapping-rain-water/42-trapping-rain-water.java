32a969abe2c85ca333cbb2e544d1654ea887654b
brwnsugr <eunho@tumblbug.com>
2022-04-07 20:27:12 +0900

    Time: 11 ms (14.23%), Space: 42 MB (99.42%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,20 +1,28 @@
 class Solution {
   public int trap(int[] height) {
+    // if we meet higher wall than previous height,
+    // then we move left til we meet higher than height[current]
+    //
+    //
     Stack<Integer> s = new Stack<>();
-    int area = 0;
-    int current = 0;
-    while(current < height.length) {
-      while(!s.isEmpty() && height[current] > height[s.peek()]) {
-        int previousIdx = s.peek();
+    int answer = 0;
+    int currIdx = 0;  // [1,0,1]
+    s.add(currIdx); // s = [0, 1]
+    while(!s.isEmpty() && currIdx < height.length - 1) {
+      currIdx++;
+      // int currHeight = height[s.top()];
+      while (!s.isEmpty() && height[s.peek()] < height[currIdx]) {
+        int currHeight = height[s.peek()];
         s.pop();
         if(s.isEmpty()) break;
-        int w = current - s.peek() -1;
-        int h = Math.min(height[current], height[s.peek()]) - height[previousIdx];
-        area += w*h;
+        int width = currIdx - s.peek() - 1;
+        int heightDiff = Math.min(height[currIdx], height[s.peek()]) - currHeight;
+        answer += (width * heightDiff);
       }
-      s.add(current);
-      current++;
+      s.add(currIdx);
     }
-    return area;
+
+    return answer;
+
   }
 }
