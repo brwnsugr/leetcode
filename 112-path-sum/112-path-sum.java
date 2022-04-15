3c041f95787000a496f784a96bf18c9ce7230933
brwnsugr <eunho@tumblbug.com>
2022-04-15 09:14:45 +0900

    Time: 0 ms (100.00%), Space: 42.4 MB (81.93%) - LeetHub

--- 112-path-sum/112-path-sum.java
+++ 112-path-sum/112-path-sum.java
@@ -14,22 +14,39 @@
  * }
  */
 class Solution {
+     
+    private boolean targetSumMeet = false;
+    private int target;
     
-    boolean pathSum;
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        pathSum = false;
+        // root -> leaf : val sum sum == target true
+        //       5 -> left, right 
+        //.    3(leaf)  2 
+        //       currNode.left , right == null  
+        // TC: O(N),  
+        //
+        target = targetSum;
+        // targetSumMeet
+        //[2], targetSum = 2;
+        //.  5
+        //  3 2^  ,,   targetSum = 7
+        //
         if(root == null) return false;
-        dfs(root, root.val, targetSum);
-        return pathSum;
+        recursive(root, root.val);
+        return targetSumMeet;
     }
     
-    private void dfs(TreeNode curr, int sum, int targetSum) {
+    private void recursive(TreeNode curr, int sum) {
+        //leaf를 만났을 때 
         if(curr.left == null && curr.right == null) {
-            if(sum == targetSum) pathSum = true;
+            if(sum == target) { // sum = 7
+                targetSumMeet = true;
+            }
             return;
         }
         
-        if(curr.left != null) dfs(curr.left, sum + curr.left.val, targetSum);
-        if(curr.right != null) dfs(curr.right, sum + curr.right.val, targetSum);
+        if(curr.left != null) recursive(curr.left, sum + curr.left.val);
+        
+        if(curr.right != null) recursive(curr.right, sum + curr.right.val);
     }
 }
