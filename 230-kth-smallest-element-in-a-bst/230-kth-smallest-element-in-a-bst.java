7411e316d4ebd29180c6752a112d7ec91644a6f7
brwnsugr <eunho@tumblbug.com>
2022-04-22 09:28:53 +0900

    Time: 0 ms (100.00%), Space: 42.1 MB (82.74%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
@@ -17,25 +17,25 @@ class Solution {
     
     private int count = 0;
     private int answer = 0;
-    private boolean kFound = false;
+    // private boolean kFound = false;
     
     public int kthSmallest(TreeNode root, int k) {
         // approach: in order traverse 해서, k번째 순회 시점의 element를 답으로 낸다.
         // k > # of nodes 
+        // TC: 
         
+        //.  # of nodes = k 
         inOrder(root, k); 
         return answer;
     }
     
     private void inOrder(TreeNode root, int k) {
-        if(kFound) return;
         
         if(root != null) {
             inOrder(root.left, k);
             //visit node
             count++;
             if(count == k) {
-                kFound = true;
                 answer = root.val;
                 return;
             }
