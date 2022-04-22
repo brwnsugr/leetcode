68cd10de1d8c5e58f80c101f334bed440d716894
brwnsugr <eunho@tumblbug.com>
2022-04-22 09:52:35 +0900

    Time: 35 ms (41.09%), Space: 67.8 MB (12.92%) - LeetHub

--- 77-combinations/77-combinations.java
+++ 77-combinations/77-combinations.java
@@ -1,34 +1,51 @@
 class Solution {
     
-    private int targetCount;
-    private int maxNum;
-    private List<List<Integer>> answers = new ArrayList<>();
+    List<List<Integer>> answers = new ArrayList<>();
+    private int targetK;
+    
     
     public List<List<Integer>> combine(int n, int k) {
-        targetCount = k;
-        maxNum = n;
-        LinkedList<Integer> path = new LinkedList<>();
-        recursive(1, 0, path);
+        // 
+        // 4P2 = 4C2 * 2!  
+        // 1,2,3,     x-> (2 ->(3 or x) or x -> ) ->
+        //            1 -> (2 or x)
+        // 
+        //   1,2
+        // n = 4,, k = 2
+        // -> 1^,2^,3,4          pickedCount = 0, 
+        // [1,2]              pickedCount = 2
+        // O(2^N) -> 
+        //
+        targetK = k;
+        LinkedList<Integer> paths = new LinkedList<>();
+        comb(n, 1, 0, paths);
         return answers;
     }
     
-    private void recursive(int curr, int count, LinkedList<Integer> path) {
+    
+    private void comb(int n, int currNumber, int pickedCount, LinkedList<Integer> paths) {
         
-        if(curr == maxNum +1) {
-            if(count == targetCount) {
-                answers.add(new ArrayList<>(path));
-            }
+        if(pickedCount == targetK) {
+            answers.add(new ArrayList<>(paths));
             return;
         }
         
-        else {
-            //원소 넣기
-            path.addLast(curr);
-            recursive(curr+1, count+1, path);
-            path.removeLast();
-            
-            //원소 빼기 
-            recursive(curr+1, count, path);
+        if(currNumber > n) {
+            return;
         }
+        
+        
+        //1. 현재꺼를 pick
+        paths.addLast(currNumber); // n = 4, k = 2.  [3, 4] 2 paths = [1]
+        comb(n, currNumber+1, pickedCount+1, paths);
+        paths.removeLast();
+        
+        //2. 현재꺼를 pick X
+        comb(n, currNumber+1, pickedCount, paths);
+        
+        
     }
+    
+    
+    
 }
