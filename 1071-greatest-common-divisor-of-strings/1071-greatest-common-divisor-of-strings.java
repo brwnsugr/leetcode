b7bb0a0f170297c74c310178d507429769d1a6bc
brwnsugr <eunho@tumblbug.com>
2022-04-29 23:28:53 +0900

    Time: 5 ms (20.14%), Space: 42.1 MB (82.79%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -1,19 +1,21 @@
 class Solution {
     public String gcdOfStrings(String str1, String str2) {  //str1 "ABCABC", str2 "ABC" -> 
-        if(str1.length() < str2.length()) {
-            return gcdOfStrings(str2, str1);
+        String concatStr = str1 + str2;
+        String reverseConcatStr = str2 + str1;
+        if(!concatStr.equals(reverseConcatStr)) return "";
+        int gcd = getGCD(str1.length(), str2.length());
+        System.out.println("gcd is " + gcd);
+        return str1.substring(0, gcd);
+    }
+    
+    
+    private int getGCD(int a, int b) {
+        while(b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
         }
-        
-        else if(!str1.substring(0, str2.length()).equals(str2)) {
-            return "";
-        }
-        
-        else if(str2.equals("")) return str1;
-        
-        
-        else {
-            return gcdOfStrings(str1.substring(str2.length()), str2);
-        }
+        return a;
     }
     
     
