6bb745645f54f1d9faf53531abad7a276c7e778c
brwnsugr <eunho@tumblbug.com>
2022-05-02 21:50:50 +0900

    Time: 379 ms (5.10%), Space: 45.1 MB (28.93%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.java
+++ 152-maximum-product-subarray/152-maximum-product-subarray.java
@@ -4,18 +4,10 @@ class Solution {
         for(int i = 0; i < nums.length; i++) {
             int tempAnswer = 1;
             for(int j = i; j < nums.length; j++) {
-                if(nums[j] == 0) {
-                    answer = Math.max(answer, 0);
-                    tempAnswer = 1;
-                    continue;
-                }
                 tempAnswer *= nums[j];
                 answer = Math.max(answer, tempAnswer);
             }
         }
-        
         return answer;
     }
-    
-    //
 }
