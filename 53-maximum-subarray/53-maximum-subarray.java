7b46a00d2cc4591e210e152f7401f494a212e45c
brwnsugr <eunho@tumblbug.com>
2022-05-02 21:23:40 +0900

    Time: 14 ms (5.64%), Space: 73.1 MB (64.76%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -1,14 +1,40 @@
 class Solution {
     public int maxSubArray(int[] nums) {
         //O(N^2)
-        int answer = Integer.MIN_VALUE;
-        int sum = 0; // [1,-2,3]
-        for(int i = 0; i < nums.length; i++) {
-            sum += nums[i]; 
-            answer = Math.max(answer, sum);
-            if(sum < 0) sum = 0;
-            
+
+        return getMaxConsecutiveSum(0, nums.length-1, nums);
+    }
+    
+    
+    private int getMaxConsecutiveSum(int left, int right, int[] nums) {
+        
+        if(left > right) return Integer.MIN_VALUE;
+        
+        int mid = (left + right) / 2;
+        
+        // left Biggest Sum 
+        int leftBiggestSum = 0;
+        int leftTemp = 0;
+        for(int i = mid - 1; i >= left; i--) {
+            leftTemp += nums[i];
+            leftBiggestSum = Math.max(leftBiggestSum, leftTemp);
         }
-        return answer;
+        
+        int rightBiggestSum = 0;
+        int rightTemp = 0;
+        for(int j = mid + 1; j <= right; j++) {
+            rightTemp += nums[j];
+            rightBiggestSum = Math.max(rightBiggestSum, rightTemp);
+        }
+        
+        int bestSum = nums[mid] + leftBiggestSum + rightBiggestSum;
+        
+        int leftHalf = getMaxConsecutiveSum(left, mid -1, nums);
+        int rightHalf = getMaxConsecutiveSum(mid+1, right, nums);
+        
+        return Math.max(leftHalf, Math.max(rightHalf, bestSum));
+        
+        // right Biggest Sum
+        
     }
 }
