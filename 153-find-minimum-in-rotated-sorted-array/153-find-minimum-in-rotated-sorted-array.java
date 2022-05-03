878d2c2261f5f09e37aeef57315ed52491239840
brwnsugr <eunho@tumblbug.com>
2022-05-03 22:23:13 +0900

    Time: 0 ms (100.00%), Space: 41.7 MB (95.16%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.java
@@ -1,27 +1,13 @@
 class Solution {
     public int findMin(int[] nums) {
-        
-        // find the bottom edge point 
-        // [1l,2,0lr], mid = 1
         int l = 0;
         int r = nums.length - 1;
-        int answer = nums[0];
-        
-        int mid = 0;
+        //[0,2,3]  , l=0,r=1, m=0
         while(l < r) {
-            mid = l + (r-l)/2;
-            
-            if(nums[mid] > nums[r]) {
-                l = mid+1;
-                answer = nums[l];
-            }
-            else {
-                r = mid;
-                answer = nums[r];
-            }
+            int mid = l + (r-l)/2;
+            if(nums[mid] > nums[r]) l = mid+1;
+            else r = mid;
         }
-        
         return nums[l];
-    
     }
 }
