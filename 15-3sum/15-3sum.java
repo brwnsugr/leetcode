c60be3f2807e97f7e891721446b6389124477635
brwnsugr <eunho@tumblbug.com>
2022-05-05 00:06:11 +0900

    Time: 302 ms (26.36%), Space: 46.3 MB (93.78%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,22 +1,30 @@
 class Solution {
-  public List<List<Integer>> threeSum(int[] nums) {
-    Set<List<Integer>> resSet = new HashSet<>();
-    Set<Integer> firstDups = new HashSet<>();
-    Map<Integer, Integer> seen = new HashMap<>();
     
-    for(int i = 0; i < nums.length - 2; i++) {
-      if(firstDups.add(nums[i])) {
-        for(int j = i+1; j < nums.length; j++) {
-          int complement = -nums[i] -nums[j];
-          if(seen.containsKey(complement) && seen.get(complement) == i) {
-            List<Integer> res = Arrays.asList(complement, nums[i], nums[j]);
-            Collections.sort(res);
-            resSet.add(res);
-          }
-          seen.put(nums[j], i);
+    List<List<Integer>> res = new ArrayList<>();
+    public List<List<Integer>> threeSum(int[] nums) {
+        // n_C_3 = n n-1 n-2 -> O(N^3)
+        
+        Arrays.sort(nums);
+        
+        for(int i = 0; i < nums.length - 2; i++) {
+            int first = nums[i];
+            int complement = -first;
+            if(i > 0 && nums[i-1] == nums[i]) continue;
+            checkRestSum(i+1, nums, complement);
         }
-      }
+        
+        return res;
     }
-    return new ArrayList<>(resSet);
-  }
+    
+    private void checkRestSum(int start, int[] nums, int target) {
+        Set<Integer> visited = new HashSet<>();
+        for(int i = start; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if(visited.contains(complement)) {
+                res.add(Arrays.asList(nums[start-1], complement, nums[i]));
+                while(i < nums.length -1 &&nums[i] == nums[i+1]) i++;
+            }
+            visited.add(nums[i]);
+        }
+    }
 }
