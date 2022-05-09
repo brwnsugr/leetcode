d0e874b6c32ddac3e7f53de9e316dd341a7d07b3
brwnsugr <eunho@tumblbug.com>
2022-05-09 21:05:30 +0900

    Time: 5 ms (88.02%), Space: 42.4 MB (86.49%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -1,20 +1,25 @@
 class Solution {
-  public int lengthOfLongestSubstring(String s) {
-    if(s.length() <= 1) return s.length();
-
-    int anchor = -1;
-    int ans = 0;
-
-    Map<Character, Integer> m = new HashMap<>();
-
-    for(int i = 0; i < s.length(); i++) {
-      if(m.containsKey(s.charAt(i))
-          && anchor < m.get(s.charAt(i))) {
-        anchor = m.get(s.charAt(i));
-      }
-      m.put(s.charAt(i), i);
-      ans = Math.max(ans, i - anchor);
+    public int lengthOfLongestSubstring(String s) {
+        //Naive Approach:
+        // 
+        // abcabcbb
+        // aaabcda -> store Character and its index using hashmap
+        //when we're reaching out to duplicated characters
+        // move the anchor to last index of currChar or 
+        // a: 0, a:1, a:2, b: 3 (len = 1-0 = 1), currLen = 1++;
+        if(s.length() == 0) return 0;
+        Map<Character, Integer> charIdxMap = new HashMap<>();
+        int answer = Integer.MIN_VALUE;
+        int pivot = -1;
+        for(int i = 0; i < s.length(); i++) {
+            
+            char currChar = s.charAt(i);  //      aabc
+            if(charIdxMap.containsKey(currChar)) { // a: 0, a: 1 currlen = 1 
+                pivot = Math.max(pivot, charIdxMap.get(currChar));
+            }
+            answer = Math.max(i-pivot, answer);
+            charIdxMap.put(currChar, i);
+        }
+        return answer;
     }
-    return ans;
-  }
 }
