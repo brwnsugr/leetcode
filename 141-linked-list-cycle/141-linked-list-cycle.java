10d661781f7af36cdd3047877506b00ab947599f
brwnsugr <eunho@tumblbug.com>
2022-05-16 23:28:25 +0900

    Time: 0 ms (100.00%), Space: 46 MB (37.03%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.java
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -11,14 +11,22 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        Set<ListNode> visited = new HashSet<>();
         
-        while(head != null) {
-            if(visited.contains(head)) return true;
-            visited.add(head);
-            head = head.next;
+        // cyclic하게, Fast와 slow 가 돌다보면 결국 만남 
+        // why must meet, if it's cycle?
+        // 속도의 차이 때문임. cyclic하고, 속도의 차이가 1이면 결국 만날수밖에 없음. 시침, 분침이 한번은 만나는 것과 똑같음
+        // worst case 일 때는, 
+        if(head == null) return false;
+        ListNode slow = head;
+        ListNode fast = head.next;
+        
+        while(slow != fast) {
+            if(fast == null || fast.next == null) return false;
+            
+            fast = fast.next.next;
+            slow = slow.next;
         }
         
-        return false;
+        return true;
     }
 }
