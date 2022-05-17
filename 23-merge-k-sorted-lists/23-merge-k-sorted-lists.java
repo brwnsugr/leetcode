a247ce541d01ab218e4a6fc713d313e7e668ab6e
brwnsugr <eunho@tumblbug.com>
2022-05-17 21:29:56 +0900

    Time: 740 ms (5.01%), Space: 319.6 MB (5.25%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
@@ -9,37 +9,44 @@
  * }
  */
 class Solution {
+    
+    private ListNode retNode;
+    private ListNode dummy;
+    
     public ListNode mergeKLists(ListNode[] lists) {
-        // [1->2->3], [2->3->4],
-        ListNode dummy = new ListNode(0);
-        ListNode point = dummy;
-        while(true) {
-            boolean allEmpty = true;
-            int minListPos = 0;
-            int min = Integer.MAX_VALUE;
-            for(int i = 0; i < lists.length; i++) {
-                if(lists[i] != null) {
-                    
-                    int val = lists[i].val;
-                    if(val < min) {
-                        min = val;
-                        minListPos = i;
-                    }
-                    allEmpty = false;
-                }
-            }
-            if(allEmpty) break;
-            
-            // add item to the result LL
-            
-            point.next = new ListNode(min);
-            
-            //pointer move to r
-            lists[minListPos] = lists[minListPos].next;
-            
-            //point move on to r
-            point = point.next;
+        // merge lists one by one
+        // O(N * k) -> //merge between 2 lists -> (k-1) times, 
+        // and per each merge operation, it takes O(N) in the worst case
+        // (20 + 40 + 60 + 100)
+        if(lists.length == 0) return null;
+        if(lists.length == 1) return lists[0];
+        ListNode mergeNode = lists[0];
+        for(int i = 0; i < lists.length - 1; i++) {
+            mergeNode = mergeTwoNodes(mergeNode, lists[i+1]);
         }
-        return dummy.next;
+        return mergeNode;
+     }
+    
+    
+    private ListNode mergeTwoNodes(ListNode l1, ListNode l2) {
+        ListNode h = new ListNode(0);
+        ListNode ret = h;
+        
+        while(l1 != null || l2 != null) {
+            Integer l1Val = l1 == null ? Integer.MAX_VALUE : l1.val;
+            Integer l2Val = l2 == null ? Integer.MAX_VALUE : l2.val;
+            if(l1Val < l2Val) {
+                h.next = new ListNode(l1Val);
+                h = h.next;
+                l1 = l1.next;
+            }
+            else {
+                h.next = new ListNode(l2Val);
+                l2 = l2.next;
+                h = h.next;
+            }
+        }
+        
+        return ret.next;
     }
 }
