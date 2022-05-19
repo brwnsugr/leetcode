e86cdb2a7e608b3d1a665b1afaaab4f7efb17ff7
brwnsugr <eunho@tumblbug.com>
2022-05-19 09:14:43 +0900

    Time: 4 ms (53.67%), Space: 42.5 MB (83.88%) - LeetHub

--- 957-prison-cells-after-n-days/957-prison-cells-after-n-days.java
+++ 957-prison-cells-after-n-days/957-prison-cells-after-n-days.java
@@ -1,41 +1,63 @@
 class Solution {
-  public int[] prisonAfterNDays(int[] cells, int n) {
-    Map<Integer, Integer> s = new HashMap<>();
-    boolean fastForward = false;
-    while(n > 0) {
-      if(!fastForward) {
-        if(s.containsKey(convertToBitMap(cells))) {
-          n = n % (s.get(convertToBitMap((cells))) - n);
-          fastForward = true;
+    Map<Integer, Integer> visitedMap = new HashMap<>();
+    public int[] prisonAfterNDays(int[] cells, int n) {
+        
+        int cycle = 0;
+        int startIdx = 0;
+        for(int i = 0; i <= n; i++) {
+            int val = covertToBinary(cells);
+            if(!visitedMap.containsKey(val)) {
+                // System.out.println("cells is " + cells[0] +cells[1] +cells[2]+cells[3]+cells[4]+cells[5]+cells[6]+cells[7] + " and i " + i);
+                visitedMap.put(val, i);
+            }
+            else {
+                cycle = i - visitedMap.get(val);
+                startIdx = visitedMap.get(val);
+                break;
+            }
+            cells = convert(cells).clone();
+            
         }
-        else {
-          s.put(convertToBitMap(cells), n);
+        if(cycle != 0) n = startIdx + (n-startIdx) % cycle;
+        
+        for (Map.Entry<Integer, Integer> m : visitedMap.entrySet()){
+            if(m.getValue() == n) {
+                return binaryToCells(m.getKey());
+            }
+        } 
+        return null;
+    }
+    
+    private int[] convert(int[] cells) {
+        int[] convertedCell = new int[8];
+        for(int i = 1; i < 7; i++) {
+            if(cells[i-1] == cells[i+1]) convertedCell[i] = 1;
         }
-      }
-      if(n > 0) {
-        n--;
-        cells = call(cells);
-      }
+        return convertedCell;
     }
-    return cells;
-  }
-
-  private int[] call(int[] cells) {
-    int[] newCells = new int[cells.length];
-    newCells[0] = 0;
-    newCells[cells.length-1] = 0;
-    for(int i = 1; i < cells.length -1; i++) {
-      newCells[i] = (cells[i-1] == cells[i+1]) ? 1 : 0;
+    
+    private int covertToBinary(int[] convertedCell) {
+        int ret = 0;
+        for(int i = convertedCell.length-1; i >= 0; i--) {
+            int temp = convertedCell[i] == 1 ? (int) Math.pow(2, 7-i) : 0;
+            ret += temp;
+        }
+        return ret;
     }
-    return newCells;
-  }
-
-  private int convertToBitMap(int[] cells) {
-    int stateBitMap = 0;
-    for (int cell : cells) {
-      stateBitMap <<= 1;
-      stateBitMap = (stateBitMap | cell);
+    
+    private int[] binaryToCells(int num) {
+        int start = 7;
+        int[] ret = new int[8];
+        while(start >= 0) {
+            int digitNum =(int) Math.pow(2, start);
+            if(num >= digitNum && num != 0) {
+                num -= digitNum;
+                ret[7-start] = 1;
+            }
+            start--;
+        }
+        return ret;
     }
-    return stateBitMap;
-  }
+    
+    
 }
