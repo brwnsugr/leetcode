024b518f2513ffc500fa9daa41b2040eccb8f00c
brwnsugr <eunho@tumblbug.com>
2022-05-23 23:07:15 +0900

    Time: 3 ms (91.73%), Space: 42.3 MB (92.92%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -6,13 +6,12 @@ class Solution {
         
         for(int i = 0; i < s.length(); i++) {
             count[s.charAt(i) - 'a']++;
-        }
-        
-        for(int i = 0; i < t.length(); i++) {
             count[t.charAt(i) - 'a']--;
-            if(count[t.charAt(i) - 'a'] < 0) return false;
         }
         
+        for(int i = 0; i < 26; i++) {
+            if(count[i] != 0) return false;
+        }
         return true;
     }
 }
