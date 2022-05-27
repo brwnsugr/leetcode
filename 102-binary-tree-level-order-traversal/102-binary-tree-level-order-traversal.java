26cc90993d0054386c8d74ac2117dd8836daa9bb
brwnsugr <eunho@tumblbug.com>
2022-05-28 00:28:59 +0900

    Time: 1 ms (85.27%), Space: 44 MB (18.00%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
@@ -14,20 +14,27 @@
  * }
  */
 class Solution {
-    
-    private List<List<Integer>> answer = new ArrayList<>();
-    
     public List<List<Integer>> levelOrder(TreeNode root) {
         if(root == null) return new ArrayList<>();
-        recursive(root, 0);
+        List<List<Integer>> answer = new ArrayList<>();
+        
+        Queue<TreeNode> q = new LinkedList<>();
+        
+        q.add(root);
+        
+        while(!q.isEmpty()) {
+            int qSize = q.size();
+            List<Integer> level = new ArrayList<>();
+            for(int i = 0; i < qSize; i++) {
+                TreeNode curr = q.poll();
+                level.add(curr.val);
+                if(curr.left != null) q.add(curr.left);
+                if(curr.right != null) q.add(curr.right);
+            }    
+            answer.add(level);
+        }
+        
+        
         return answer;
-    }
-    
-    private void recursive(TreeNode curr, int level) {
-        if(level == answer.size()) answer.add(new ArrayList<>());
-        answer.get(level).add(curr.val);
-        if(curr.left != null) recursive(curr.left, level + 1);
-        if(curr.right != null) recursive(curr.right, level + 1);
-        return;
     }
 }
