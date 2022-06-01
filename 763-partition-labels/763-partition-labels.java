dd99035e140e11110f323ae097fbce0d810212c3
brwnsugr <eunho@tumblbug.com>
2022-06-02 01:01:10 +0900

    Time: 5 ms (72.31%), Space: 42 MB (80.09%) - LeetHub

--- 763-partition-labels/763-partition-labels.java
+++ 763-partition-labels/763-partition-labels.java
@@ -1,21 +1,25 @@
 class Solution {
-  public List<Integer> partitionLabels(String s) {
-    int[] last = new int[26];
-
-    for(int i = 0; i < s.length(); i++) {
-      last[s.charAt(i) - 'a'] = i;
+    public List<Integer> partitionLabels(String s) {
+        Map<Character, Integer> rightMostIdxMap = new HashMap<>();
+        int idx = 0;
+        for(char c : s.toCharArray()) {
+            rightMostIdxMap.put(c, idx++);
+        }
+        List<Integer> partitions = new ArrayList<>();
+        int maxIdx = 0;
+        for(int i = 0; i < s.length(); i++) {
+            maxIdx = Math.max(maxIdx, rightMostIdxMap.get(s.charAt(i)));
+            while( i < maxIdx) {
+                maxIdx = Math.max(maxIdx, rightMostIdxMap.get(s.charAt(i)));
+                i++;
+            }
+            partitions.add(maxIdx+1);
+        }
+        
+        for(int i = partitions.size() -1; i > 0; i--) {
+            int diff = partitions.get(i) - partitions.get(i-1);
+            partitions.set(i, diff);
+        }
+        return partitions;
     }
-    
-    int start = 0;
-    int end = 0;
-    List<Integer> ret = new ArrayList<>();
-    for(int i = 0; i < s.length(); i++) {
-      end = Math.max(end, last[s.charAt(i) -'a']);
-      if(i == end) {
-        ret.add(end - start + 1);
-        start = end + 1;
-      }
-    }
-    return ret;
-  }
 }
