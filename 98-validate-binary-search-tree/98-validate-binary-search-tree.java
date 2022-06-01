99e7f6d6544df207c59184c05a6d18586980f6c8
brwnsugr <eunho@tumblbug.com>
2022-06-01 17:28:17 +0900

    Time: 1 ms (52.61%), Space: 43.8 MB (54.29%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -14,27 +14,25 @@
  * }
  */
 class Solution {
-  public boolean isValidBST(TreeNode root) {
-
-    /**
-     * We traverse 1. root 2. left sub tree, 3. right sub tree in order.
-     * and everytime we traverse each node, do logic above recursively.
-     *
-     * once if we would find that from the view of curr node that we're at, its left sub is bigger or right sub is less than curr node,
-     * then return false;
-     *
-     * after All Traversal, without any returning false,
-     * then it's obvious that return true
-     */
-
-    return isValid(root, null, null);
-  }
-
-  private boolean isValid(TreeNode root, Integer low, Integer high) {
-    if(root == null) return true;
-
-    if((low != null && low >= root.val) ||(high != null && high <= root.val)) return false;
-
-    return isValid(root.left, low ,root.val) && isValid(root.right, root.val, high);
-  }
+    
+    // private Integer lowerBound = Integer.MIN_VALUE;
+    // private int upperBound = Integer.MAX_VALUE;
+    
+    public boolean isValidBST(TreeNode root) {
+        return isValid(root, null, null);
+    }
+    
+    private boolean isValid(TreeNode root, Integer lower, Integer upper) {
+        if(root == null) return true;
+        else if((lower != null && root.val <= lower) 
+                || (upper != null && root.val >= upper)) {
+            return false; 
+        } 
+        else{
+            boolean isLeftValid = isValid(root.left, lower, root.val);
+            boolean isRightValid = isValid(root.right, root.val, upper);
+            return isLeftValid && isRightValid;
+        }
+        
+    }
 }
