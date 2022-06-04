2a47353f26c5e39a80fa2c2385e7cdbe594561ac
brwnsugr <eunho@tumblbug.com>
2022-06-04 10:06:15 +0900

    Time: 6 ms (94.96%), Space: 73.5 MB (5.18%) - LeetHub

--- 713-subarray-product-less-than-k/713-subarray-product-less-than-k.java
+++ 713-subarray-product-less-than-k/713-subarray-product-less-than-k.java
@@ -1,10 +1,9 @@
 class Solution {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
-        if(k<=1) return 0;
         int prod = 1;
-        
-        int l = 0;
         int answerCount = 0;
+        if(k <= 1) return 0;
+        int l = 0;
         for(int r = 0; r < nums.length; r++) {
             prod *= nums[r];
             while(prod >= k) {
@@ -12,7 +11,6 @@ class Solution {
             }
             answerCount += r - l + 1;
         }
-        
         return answerCount;
     }
 }
