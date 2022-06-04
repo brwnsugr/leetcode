ffb4c714bd23a56790c230d42fbe9769c437e840
brwnsugr <eunho@tumblbug.com>
2022-06-04 20:54:42 +0900

    Time: 9 ms (52.82%), Space: 47.2 MB (51.65%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
@@ -8,47 +8,13 @@
  * }
  */
 class Solution {
-    private int foundCount;
-    private boolean allFound;
-    private List<Integer> pathP = new ArrayList<>();
-    private List<Integer> pathQ = new ArrayList<>();
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        foundCount = 0;
-        allFound = false;
-        TreeNode curr = root;
-        findNode(curr, p, new ArrayList<>(), pathP);
-        findNode(curr, q, new ArrayList<>(), pathQ);
-        
-        Set<Integer> pVisit = new HashSet<>(pathP);
-        int ancestorValue = 0;
-        for(int i = pathQ.size() - 1; i >= 0; i--) {
-            int qItem = pathQ.get(i);
-            if(pVisit.contains(qItem)){
-                ancestorValue = qItem;
-                break;
-            }
-        }
-        return getNodeByVal(curr, ancestorValue);
-    }
-    
-    private TreeNode getNodeByVal(TreeNode curr, int value) {
-        if(curr == null) return null;
-        if(curr.val == value) return curr;
-        TreeNode leftNodeToReturn = getNodeByVal(curr.left, value);
-        TreeNode rightNodeToReturn = getNodeByVal(curr.right, value);
-        if(leftNodeToReturn != null) return leftNodeToReturn;
-        else return rightNodeToReturn;
-    }
-    
-    private void findNode(TreeNode curr, TreeNode target, List<Integer> tempPath, List<Integer> path) {
-        if(curr == null) return;
-        tempPath.add(curr.val);
-        if(curr.val == target.val) {
-            path.addAll(tempPath);
-            return;
-        }
-        findNode(curr.left, target, tempPath, path);
-        findNode(curr.right, target, tempPath, path);
-        tempPath.remove(tempPath.size() - 1);
+        if(root == null) return null;
+        if(root == p) return root;
+        if(root == q) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if(left != null && right != null) return root;
+        return left == null ? right : left;
     }
 }
