5bf900204fbad0729d4d58214ae9da17906df04d
brwnsugr <eunho@tumblbug.com>
2022-06-09 09:17:01 +0900

    Time: 8 ms (25.40%), Space: 48.7 MB (14.78%) - LeetHub

--- 261-graph-valid-tree/261-graph-valid-tree.java
+++ 261-graph-valid-tree/261-graph-valid-tree.java
@@ -1,37 +1,42 @@
 class Solution {
-    
-    
     public boolean validTree(int n, int[][] edges) {
-        Map<Integer, Integer> parentMap = new HashMap<>();
+        List<List<Integer>> adjList = new ArrayList<>();
         
-        List<List<Integer>> adjList = new ArrayList<>(n);
         for(int i = 0; i < n; i++) adjList.add(new ArrayList<>());
-        
-        for(int[] edge : edges){
-            adjList.get(edge[0]).add(edge[1]);
-            adjList.get(edge[1]).add(edge[0]);
+        for(int[] edge : edges) {
+            int prev = edge[0];
+            int dest = edge[1];
+            adjList.get(prev).add(dest);
+            adjList.get(dest).add(prev);
         }
         
+        Map<Integer, Integer> parentMap = new HashMap<>();
+        parentMap.put(0, -1);
         Stack<Integer> st = new Stack<>();
-        
-        parentMap.put(0,-1);
-        
         
         st.add(0);
         
         while(!st.isEmpty()) {
             int curr = st.pop();
-            
-            for(int neighbor : adjList.get(curr)){
-                if(parentMap.get(curr)==neighbor) continue;
+            for(int neighbor : adjList.get(curr)) {
+                if(parentMap.get(curr) == neighbor) continue;
                 if(parentMap.containsKey(neighbor)) return false;
-                
                 st.add(neighbor);
                 parentMap.put(neighbor, curr);
             }
         }
         
-        
         return parentMap.size() == n;
     }
 }
+
+/**
+
+        1(n)
+       2^ 3
+      4(n)
+
+map(2: 1, )
+
+
+**/
