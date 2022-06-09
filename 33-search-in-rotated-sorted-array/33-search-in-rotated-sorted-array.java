c5b243f7bd6a353512e8d9fd8ffd8960fd8ecda9
brwnsugr <eunho@tumblbug.com>
2022-06-10 01:05:27 +0900

    Time: 1 ms (62.37%), Space: 42.1 MB (70.29%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -6,7 +6,7 @@ class Solution {
             int mid = l + (r-l) / 2;
             if(nums[mid] == target) return mid;
             else if(nums[mid] < nums[r]) {
-                if(target > nums[mid] && target <= nums[r]) {
+                if(target >= nums[mid] && target <= nums[r]) {
                     l = mid+1;
                 } 
                 else {
