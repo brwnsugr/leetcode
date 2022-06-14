1f4fa7855e8f12ae07b1d75760c4e4f76dbf3e02
brwnsugr <eunho@tumblbug.com>
2022-06-14 22:06:56 +0900

    Time: 9 ms (81.19%), Space: 42.5 MB (51.66%) - LeetHub

--- 494-target-sum/494-target-sum.java
+++ 494-target-sum/494-target-sum.java
@@ -1,22 +1,39 @@
 class Solution {
     
     private int count = 0;
+    private int total = 0;
+    private int[][] cache;
     
     public int findTargetSumWays(int[] nums, int target) {
-        calculate(nums, 0, 0, target);
-        return count;
+        for(int num : nums) total += num;
+        cache = new int[nums.length][total*2 + 1];
+        for(int[] cacheRow : cache) {
+            Arrays.fill(cacheRow, -1);
+        }
+        return calculate(nums, 0, 0, target);
     }
     
-    private void calculate(int[] nums, int i , int sum, int S) {
+    private int calculate(int[] nums, int i , int sum, int S) {
         if(i == nums.length) {
             if(sum == S) {
-                count++;
+                return 1;
+            }
+            else {
+                return 0;
             }
         }
         
         else {
-            calculate(nums, i+1, sum + nums[i], S);
-            calculate(nums, i+1, sum - nums[i], S);
+            if(cache[i][sum + total] != -1) {
+                return cache[i][sum + total];
+            }
+            else {
+                int add = calculate(nums, i+1, sum + nums[i], S);
+                int subtract = calculate(nums, i+1, sum - nums[i], S);
+                
+                cache[i][sum + total] = add+subtract;
+                return cache[i][sum + total];
+            }
         }
     }
 }
