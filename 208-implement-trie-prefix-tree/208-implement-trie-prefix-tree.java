57b0542814cd3b56e6146cb5ba55ef8a66400660
brwnsugr <eunho@tumblbug.com>
2022-06-15 21:42:51 +0900

    Time: 50 ms (72.02%), Space: 67.4 MB (57.23%) - LeetHub

---
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.java
@@ -0,0 +1,66 @@
+class Trie {
+    
+    TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+        
+    }
+    
+    public void insert(String word) {
+        TrieNode curr = root;
+        for(int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if(curr.next[c-'a'] ==null) curr.next[c-'a'] = new TrieNode();
+            if(i == 0) curr.next[c-'a'].isFilled = true;
+            else {
+                curr.next[c-'a'].isFilled = true;
+            }
+            curr = curr.next[c-'a'];
+        }
+        curr.isEnd = true;
+        
+    }
+    
+    public boolean search(String word) {
+        TrieNode curr = root;
+        for(int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if(curr.next[c-'a'] == null) return false;
+            else {
+                curr = curr.next[c-'a'];
+            }
+        }
+        return curr.isEnd;
+    }
+    
+    public boolean startsWith(String prefix) {
+        TrieNode curr = root;
+        for(int i = 0; i < prefix.length(); i++) {
+            char c = prefix.charAt(i);
+            if(curr.next[c-'a'] == null) return false;
+            curr = curr.next[c-'a'];
+        }
+        return true;
+    }
+}
+
+class TrieNode {
+    public TrieNode[] next;
+    public boolean isEnd;
+    public boolean isFilled;
+    
+    public TrieNode() {
+        this.next = new TrieNode[26];
+        this.isEnd = false;
+        this.isFilled = false;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
