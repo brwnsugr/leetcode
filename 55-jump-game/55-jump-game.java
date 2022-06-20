699230d578d0f28754bdf284c79303de3a769ba9
brwnsugr <eunho@tumblbug.com>
2022-06-21 00:16:05 +0900

    Time: 995 ms (6.78%), Space: 43.1 MB (84.41%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,14 +1,24 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int leftMostPos = nums.length - 1;
-        for(int i = nums.length - 1; i >= 0; i--) {
-            
-            if(i + nums[i] >= leftMostPos) {
-                leftMostPos = i;
+        Index[] memo = new Index[nums.length];
+        for (int i = 0; i < memo.length; i++) {
+            memo[i] = Index.UNKNOWN;
+        }
+        memo[memo.length - 1] = Index.GOOD;
+
+        for (int i = nums.length - 2; i >= 0; i--) {
+            int furthestJump = Math.min(i + nums[i], nums.length - 1);
+            for (int j = i + 1; j <= furthestJump; j++) {
+                if (memo[j] == Index.GOOD) {
+                    memo[i] = Index.GOOD;
+                }
             }
         }
-        
-        
-        return leftMostPos == 0;
+
+        return memo[0] == Index.GOOD;
     }
+}
+
+enum Index {
+    GOOD, BAD, UNKNOWN
 }
