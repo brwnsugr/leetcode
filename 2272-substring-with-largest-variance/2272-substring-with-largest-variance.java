12033dbd3c6ee3aa9c5533c357eba49831d04902
brwnsugr <eunho@tumblbug.com>
2022-06-24 00:27:45 +0900

    Time: 407 ms (31.51%), Space: 93.5 MB (14.50%) - LeetHub

--- 2272-substring-with-largest-variance/2272-substring-with-largest-variance.java
+++ 2272-substring-with-largest-variance/2272-substring-with-largest-variance.java
@@ -1,47 +1,40 @@
 class Solution {
     public int largestVariance(String s) {
-        int answer = 0;
         
-        for(char i = 'a'; i <= 'z'; i++) {
-            for(char j = 'a'; j <= 'z'; j++) {
-                if(i == j) continue;
+        int[] freq = new int[26];
+        for(char c : s.toCharArray()) {
+            freq[c-'a']++;
+        }
+        
+        int maxVariance = 0;
+        
+        for(int i = 0; i < 26; i++) {
+            for(int j = 0; j < 26; j++) {
+                if(i == j || freq[i] == 0 || freq[j] == 0) continue;
                 
+                int remainA = freq[i];
                 
-                char[] strArr = s.toCharArray();
-                for(int k = 0; k < 2; k++) {
-                    int count1 = 0;
-                    int count2 = 0;
-                    for(char c : strArr) {
-                        if(c == i) count1++;
-                        if(c == j) count2++;
-                        if(count1 < count2) {
-                            count1 = 0;
-                            count2 = 0;
-                        }
-                        
-                        if(count1 > 0 && count2 > 0) {
-                            
-                            answer = Math.max(answer, count1 - count2);
-                        }
+                int freqA = 0;
+                int freqB = 0;
+                for(char c : s.toCharArray()) {
+                    if((int) c-'a' == j) freqB++;
+                    if((int) c-'a' == i) {
+                        freqA++;
+                        remainA--;
                     }
                     
-                    reverse(strArr);
+                    if(freqA > 0 && freqB > 0) {
+                        maxVariance = Math.max(maxVariance, freqB - freqA);
+                    }
+                    
+                    if(freqA > freqB && remainA >= 1) {
+                        freqA = 0;
+                        freqB = 0;
+                    }
                 }
-                
             }
         }
         
-        
-        return answer;
-    }
-    
-    private void reverse(char[] chArr) {
-        int len = chArr.length;
-        // char[] chArr = s.toCharArray();
-        for(int i = 0; i < len/2; i++){
-            char temp = chArr[len-i-1];
-            chArr[len-i-1] = chArr[i];
-            chArr[i] = temp;
-        }
+        return maxVariance;
     }
 }
