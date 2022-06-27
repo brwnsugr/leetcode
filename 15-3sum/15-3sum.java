50f7d4fc508b0e50f6756b1760829b585fd3b64b
brwnsugr <eunho@tumblbug.com>
2022-06-27 22:20:15 +0900

    Time: 35 ms (51.48%), Space: 58.6 MB (69.44%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,33 +1,35 @@
 class Solution {
     
-    List<List<Integer>> res = new ArrayList<>();
+    private List<List<Integer>> res;
     public List<List<Integer>> threeSum(int[] nums) {
-        // n_C_3 = n n-1 n-2 -> O(N^3)
-        
+        res = new ArrayList<>();
         Arrays.sort(nums);
-        
-        for(int i = 0; i < nums.length - 2; i++) {
-            int first = nums[i];
-            int complement = -first;
-            if(i > 0 && nums[i-1] == nums[i]) continue;
-            checkRestSum(i+1, nums, complement);
+        for(int i = 0; i < nums.length-2; i++) {
+            int pairTarget = -nums[i];
+            if(i != 0 && nums[i] == nums[i-1]) continue;
+            buildPairTarget(pairTarget, i + 1, nums);
         }
-        
         return res;
     }
     
-    private void checkRestSum(int start, int[] nums, int target) {
-        int lo = start;
-        int hi = nums.length - 1;
+    private void buildPairTarget(int target, int start, int[] nums) {
+        int l = start;
+        int r = nums.length - 1;
         
-        while(lo < hi) {
-            int sum = nums[start-1] + nums[lo] + nums[hi];
-            if(sum < 0) lo++;
-            else if(sum > 0) hi--;
+        
+        while(l < r) {
+            int sum = nums[l] + nums[r];
+            if(sum > target) {
+                r--;
+            }
+            else if(sum < target) {
+                l++;
+            }
             else {
-                res.add(Arrays.asList(nums[start-1], nums[lo++], nums[hi--]));
-                while(lo < hi && nums[lo] == nums[lo-1]) lo++;
+                res.add(Arrays.asList(-target, nums[l++], nums[r--]));
+                while(l < r && nums[l] == nums[l-1]) l++;
             }
         }
+        
     }
 }
