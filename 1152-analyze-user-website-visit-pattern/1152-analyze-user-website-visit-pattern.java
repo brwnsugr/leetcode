b5aad7dd120fa0cf41e5ecc9dde8473209233c6e
brwnsugr <eunho@tumblbug.com>
2022-06-30 00:31:07 +0900

    Time: 41 ms (56.77%), Space: 48.6 MB (42.32%) - LeetHub

--- 1152-analyze-user-website-visit-pattern/1152-analyze-user-website-visit-pattern.java
+++ 1152-analyze-user-website-visit-pattern/1152-analyze-user-website-visit-pattern.java
@@ -1,77 +1,65 @@
 class Solution {
+    
+    
+    
     public List<String> mostVisitedPattern(String[] username, int[] timestamp, String[] website) {
-        /*
-        joe: [(home, 2), (about, 3), career]
-        james: [home, cart, maps, home]
-        mary: [home, about, career]
-        first of all, we store the each name of the user and its visitied website and timestamp as pair
-        */
-        
-        /*
-        home, about, career : 2
-        home cart maps: 1
-        home, cart, home : 1
-        home, maps, home: 1
-        cart, maps, 
-        */
-        
-        /*
-        
-        */
-        String res = "";
-        
-        int len = username.length;
-        
         Map<String, List<Pair>> userMap = new HashMap<>();
-        for(int i = 0; i < len; i++) {
+        for(int i = 0; i < username.length; i++) {
             userMap.putIfAbsent(username[i], new ArrayList<>());
-            userMap.get(username[i]).add(new Pair(website[i], timestamp[i]));
+            userMap.get(username[i]).add(new Pair(timestamp[i], website[i]));
         }
         
         Map<String, Integer> countMap = new HashMap<>();
+        String answer = "";
+        int currentSize = 0;
         
-        for(String key : userMap.keySet()) {
+        for(Map.Entry<String, List<Pair>> entry : userMap.entrySet()) {
+            List<Pair> list = entry.getValue();
+            
+            Collections.sort(list, (a, b) -> a.time - b.time);
+            int len = list.size();
             Set<String> s = new HashSet<>();
-            
-            List<Pair> userWebs = userMap.get(key);
-            
-            Collections.sort(userWebs, (a,b) -> (a.time - b.time));
-            int size = userWebs.size();
-            for(int i = 0; i < size-2; i++) {
-                for(int j = i+1; j < size - 1; j++) {
-                    for(int k = j+1; k < size; k++) {
+            for(int i = 0; i < len - 2; i++) {
+                for(int j = i + 1; j < len - 1; j++) {
+                    for(int k = j + 1; k < len; k++) {
                         StringBuilder strBuilder = new StringBuilder();
-                        strBuilder.append(userWebs.get(i).web).append(" ").append(userWebs.get(j).web).append(" ").append(userWebs.get(k).web);
-                        String temp = new String(strBuilder);
-                        if(!s.contains(temp)) {
-                            countMap.put(temp, countMap.getOrDefault(temp, 0) + 1);
-                            s.add(temp);
-                        }
+                        strBuilder.append(list.get(i).web);
+                        strBuilder.append(' ');
+                        strBuilder.append(list.get(j).web);
+                        strBuilder.append(' ');
+                        strBuilder.append(list.get(k).web);
+                        String concatWeb = new String(strBuilder);
                         
-                        if(res == "" || countMap.get(temp) > countMap.get(res) || (countMap.get(temp) == countMap.get(res) && res.compareTo(temp) >= 0)) {
-                            res = temp;
+                        if(!s.contains(concatWeb)) {
+                            countMap.put(concatWeb, countMap.getOrDefault(concatWeb, 0) + 1);
+                            s.add(concatWeb);
                         }
+                            
                         
+                        if(answer.equals("") || currentSize < countMap.get(concatWeb) 
+                           || (currentSize == countMap.get(concatWeb) && answer.compareTo(concatWeb) >= 0)) {
+                            answer = concatWeb;
+                            currentSize = countMap.get(concatWeb);
+                        }
                     }
                 }
             }
+
+            
             
             
         }
-        
-        List<String> ans = Arrays.asList(res.split(" "));
+        List<String> ans = Arrays.asList(answer.split(" "));
+        // Collections.sort(ans);
         return ans;
-        
-    }
-    
-    static class Pair{
-        String web;
-        int time;
-        
-        public Pair(String web, int time){
-            this.web = web;
-            this.time = time;
-        }
     }
 }
 
+class Pair{
+    public int time;
+    public String web;
+    public Pair(int time, String web){
+        this.time = time;
+        this.web = web;
+    }
+}
