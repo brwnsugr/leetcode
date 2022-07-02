7a4cf14e2ed97fe2b801f64368f277cd0459832d
brwnsugr <eunho@tumblbug.com>
2022-07-02 20:44:56 +0900

    Time: 2 ms (79.88%), Space: 47.7 MB (55.08%) - LeetHub

--- 57-insert-interval/57-insert-interval.java
+++ 57-insert-interval/57-insert-interval.java
@@ -1,37 +1,34 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
+        
+        List<int[]> insertedIntervals = new ArrayList<>();
+        int newStart = newInterval[0];
+        int newEnd = newInterval[1];
+        
+        //leftMost
+        
         int idx = 0;
         int len = intervals.length;
-        List<int[]> newIntervals = new ArrayList<>();
         
-        while(idx < len && intervals[idx][1] < newInterval[0]) {
-            newIntervals.add(new int[]{intervals[idx][0], intervals[idx][1]});
+        while(idx < len && intervals[idx][1] < newStart) {
+            insertedIntervals.add(new int[]{intervals[idx][0], intervals[idx][1]});
             idx++;
         }
-        int mergedLeft = newInterval[0];
-        int mergedRight = newInterval[1];
-        boolean merged = false;
-        while(idx < len && mergedRight >= intervals[idx][0]) {
-            mergedRight = Math.max(mergedRight, intervals[idx][1]);
-            mergedLeft = Math.min(mergedLeft, intervals[idx][0]);
+        
+        //merge
+        while(idx < len && intervals[idx][0] <= newEnd) {
+            newStart = Math.min(intervals[idx][0], newStart);
+            newEnd = Math.max(intervals[idx][1], newEnd);
             idx++;
         }
-        newIntervals.add(new int[]{mergedLeft, mergedRight});
+        insertedIntervals.add(new int[]{newStart, newEnd});
         
+        //rightmost
         while(idx < len) {
-            newIntervals.add(intervals[idx]);
+            insertedIntervals.add(new int[]{intervals[idx][0], intervals[idx][1]});
             idx++;
         }
         
-        
-        int[][] res = new int[newIntervals.size()][2];
-        
-        int i = 0;
-        for(int[] interval : newIntervals) {
-            res[i][0] = interval[0];
-            res[i][1] = interval[1];
-            i++;
-        }
-        return res;
+        return insertedIntervals.toArray(new int[0][0]);
     }
 }
