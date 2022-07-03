92469d498bf029877eb492ef7763c176d7c95c2c
brwnsugr <eunho@tumblbug.com>
2022-07-03 12:51:45 +0900

    Time: 11 ms (46.27%), Space: 50 MB (11.99%) - LeetHub

--- 937-reorder-data-in-log-files/937-reorder-data-in-log-files.java
+++ 937-reorder-data-in-log-files/937-reorder-data-in-log-files.java
@@ -2,18 +2,18 @@ class Solution {
     public String[] reorderLogFiles(String[] logs) {
     
         Arrays.sort(logs, new Comparator<String>(){
+           
             @Override
             public int compare(String t1, String t2) {
                 
                 String[] p1 = t1.split(" ", 2);
                 String[] p2 = t2.split(" ", 2);
-                
                 boolean p1Digit = Character.isDigit(p1[1].charAt(0));
                 boolean p2Digit = Character.isDigit(p2[1].charAt(0));
                 
                 if(!p1Digit && p2Digit) return -1;
                 else if(p1Digit && !p2Digit) return 1;
-                else if(!p1Digit && !p2Digit) {
+                else if(!p1Digit && !p2Digit){
                     if(p1[1].equals(p2[1])) {
                         return p1[0].compareTo(p2[0]);
                     }
