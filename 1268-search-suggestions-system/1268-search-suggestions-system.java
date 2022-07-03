7b7867b3a65f9eca971b430779ec250f54b5c6e2
brwnsugr <eunho@tumblbug.com>
2022-07-03 21:35:48 +0900

    Time: 324 ms (11.65%), Space: 158.1 MB (14.90%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.java
+++ 1268-search-suggestions-system/1268-search-suggestions-system.java
@@ -1,84 +1,85 @@
 class Solution {
     public List<List<String>> suggestedProducts(String[] products, String searchWord) {
-        /*
-        1. Create a trie from the given products input
-        2. 
-    
-        */
         Trie trie = new Trie();
         
-        for(String product : products) {
-            trie.insert(product);
-        }
+        for(String product : products) trie.insert(product);
         
-        String prefix = "";
         List<List<String>> answerList = new ArrayList<>();
+        String prefix = "";
+        
         
         for(char c : searchWord.toCharArray()) {
             prefix += c;
-            answerList.add(trie.getWordStartingWith(prefix));
+            List<String> res = trie.getWordStartingWith(prefix);
+            answerList.add(res);
         }
+        
         return answerList;
+    }
+    
+    
+    
+}
+
+
+class Trie {
+    
+    public Node root;
+    
+    
+    class Node {
+        public boolean isWord;
+        public Node[] children;
+        
+        public Node(){
+            children = new Node[26];
+        }
+    }
+    
+    
+    public Trie(){
+        root = new Node();
+    }
+    
+    public void insert(String word) {
+        Node curr = root;
+        
+        for(char c : word.toCharArray()) {
+            if(curr.children[c - 'a'] == null) {
+                curr.children[c-'a'] = new Node();
+            }
+            curr = curr.children[c-'a'];
+        }
+        
+        curr.isWord = true;
+    }
+    
+    private void dfsWithPrefix(Node curr, List<String> res, String word) {
+        if(res.size() == 3) return;
+        if(curr.isWord) res.add(word);
+        
+        for(int i = 0; i < 26; i++) {
+            if(curr.children[i] != null) {
+                char c = (char)(i+'a');
+                dfsWithPrefix(curr.children[i], res, word + c);
+            }
+        }
+    }
+    
+    public List<String> getWordStartingWith(String prefix) {
+        
+        Node curr = root;
+        List<String> res = new ArrayList<>();
+        
+        for(char c : prefix.toCharArray()) {
+            if(curr.children[c-'a'] == null) return res;
+            curr = curr.children[c-'a'];
+        }
+        
+        dfsWithPrefix(curr, res, prefix);
+        return res;
     }
     
     
 }
 
-class Trie {
-    class Node {
-        boolean isWord = false;
-        List<Node> children = Arrays.asList(new Node[26]);
-    }
-    
-    Node root; 
-    Node curr;
-    List<String> resultBuffer;
-    
-    void dfsWithPrefix(Node curr, String word) {
-        if(resultBuffer.size() == 3) return;
-        if(curr.isWord) resultBuffer.add(word);
-        
-        // for(char c = 'a'; c <= 'z'; c++) {
-        //     if(curr.children.get(c-'a') != null) {
-        //         dfsWithPrefix(curr.children.get(c-'a'), word + c);
-        //     }
-        // }
-        for(int i = 0; i < 26; i++) {
-            if(curr.children.get(i) != null) {
-                char c = (char) (i+'a');
-                dfsWithPrefix(curr.children.get(i), word+c);
-            }
-        }
-    }
-    
-    Trie() {
-        root = new Node();
-    }
-    
-    //Insert the String into Trie
-    void insert(String s) {
-        curr = root;
-        
-        for(char c : s.toCharArray()) {
-            if(curr.children.get(c-'a') == null) {
-                curr.children.set(c-'a', new Node());
-            }
-            curr = curr.children.get(c-'a');
-        }
-        
-        curr.isWord = true; // Mark that complete word as isWord true
-    }
-    
-    List<String> getWordStartingWith(String prefix) {
-        curr = root; // Starting from the root
-        resultBuffer = new ArrayList<>();
-        
-        for(char c : prefix.toCharArray()) {
-            if(curr.children.get(c-'a') == null) return resultBuffer;
-            curr = curr.children.get(c-'a');
-        }
-        dfsWithPrefix(curr, prefix);
-        return resultBuffer;
-    }
-    
-};
