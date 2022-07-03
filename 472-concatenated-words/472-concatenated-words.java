a4d7e0721621e8375422ee2260f9ae0441bebff5
brwnsugr <eunho@tumblbug.com>
2022-07-03 15:24:31 +0900

    Time: 134 ms (31.89%), Space: 73.6 MB (44.79%) - LeetHub

--- 472-concatenated-words/472-concatenated-words.java
+++ 472-concatenated-words/472-concatenated-words.java
@@ -1,39 +1,35 @@
 class Solution {
     public List<String> findAllConcatenatedWordsInADict(String[] words) {
-        /*
-        [cat, cats, catsdogscats, dog, dogcatsdog, rat, ratcatdogcat]
-        [cat, dog, rat, cats, catsdogscats, dogcatsdog, ratcatdogcat]
-        */
         if(words.length == 1) return new ArrayList<>();
-        
         List<String> answers = new ArrayList<>();
         Set<String> wordDict = new HashSet<>();
-        Arrays.sort(words, (String a, String b) -> a.length() - b.length());
+        
+        for(String word : words) wordDict.add(word);
         
         for(String word : words) {
             if(isValid(word, wordDict)) answers.add(word);
-            wordDict.add(word);
         }
         
         return answers;
     }
     
+    
     private boolean isValid(String word, Set<String> wordDict) {
-        boolean[] dp = new boolean[word.length()+1];
+        boolean[] dp = new boolean[word.length() + 1];
+        
         dp[0] = true;
-        //    ^  ^
-        //   "abcabc"
-        //     
-        //dp=[1001001]
         int count = 0;
         for(int i = 1; i <= word.length(); i++) {
             for(int j = 0; j < i; j++) {
-                if(dp[j] && wordDict.contains(word.substring(j, i))) {
-                    count++;
+                
+                if(dp[j] && wordDict.contains(word.substring(j, i)) && !word.substring(j,i).equals(word)) {
+                    
                     dp[i] = true;
+                    count++;
                 }
             }
         }
-        return count > 1 ? dp[word.length()] : false;
-    } 
+
+        return count > 1 ? dp[word.length()] : false;    
+    }
 }
