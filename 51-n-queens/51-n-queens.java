ee8951bf370955b52f1777d642f0a14ca78849ed
brwnsugr <eunho@tumblbug.com>
2022-07-04 00:36:56 +0900

    Time: 11 ms (28.90%), Space: 46.8 MB (17.82%) - LeetHub

--- 51-n-queens/51-n-queens.java
+++ 51-n-queens/51-n-queens.java
@@ -1,76 +1,64 @@
 class Solution {
     
-    List<List<String>> answers = new ArrayList<>();
-    
-    
-    
+    private List<List<String>> answerList;
     public List<List<String>> solveNQueens(int n) {
-        //
         
-        
-        // 1st row : Q 2nd. 
-        // 2nd r: Q 4th
-        // 3Rd: Q:1st, 
-        // 4th : Q 3rd
-        // N = length of one side length of board, O(N^2)
-        // SC: O(N^2) 
-        char[][] state = new char[n][n];
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) state[i][j] = '.';
-        }
         Set<Integer> leftDownDiagonal = new HashSet<>();
         Set<Integer> rightDownDiagonal = new HashSet<>();
         Set<Integer> visitedCol = new HashSet<>();
+        answerList = new ArrayList<>();
         
+        char[][] board = new char[n][n];
+        //init board with . 
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                board[i][j] = '.';
+            }
+        }
         
+        backTrack(0, n, leftDownDiagonal, rightDownDiagonal, visitedCol, board);
         
-        traverse(leftDownDiagonal, rightDownDiagonal, visitedCol, 0, n, state);
-            
+        return answerList;
         
-        
-        return answers;
     }
     
     
-    private void traverse(Set<Integer> leftDownDiagonal, Set<Integer> rightDownDiagonal, Set<Integer> visitedCol, int row, int n, char[][] state) {
+    private void backTrack(int row, int n, Set<Integer> leftDownDiagonal, Set<Integer> rightDownDiagonal, Set<Integer> visitedCol, char[][] board) {
         if(row == n) {
-            appendToAnswer(state);
+            appendToAnswer(board);
             return;
         }
         
         for(int col = 0; col < n; col++) {
-            
-            
-            if(!leftDownDiagonal.contains(row+col) 
-               && !rightDownDiagonal.contains(row-col) 
-               && !visitedCol.contains(col)) {
+            if(!leftDownDiagonal.contains(row + col)
+              &&!rightDownDiagonal.contains(row - col)
+              &&!visitedCol.contains(col)) {
                 leftDownDiagonal.add(row+col);
                 rightDownDiagonal.add(row-col);
                 visitedCol.add(col);
-                state[row][col] = 'Q';
-                traverse(leftDownDiagonal, rightDownDiagonal, visitedCol, row+1, n, state);
+                board[row][col] = 'Q';
+                backTrack(row+1, n, leftDownDiagonal, rightDownDiagonal, visitedCol, board);
                 leftDownDiagonal.remove(row+col);
                 rightDownDiagonal.remove(row-col);
                 visitedCol.remove(col);
-                state[row][col] = '.';
+                board[row][col] = '.';
             }
         }
+        
     }
     
-    private void appendToAnswer(char[][] state) {
-        List<String> answer = new ArrayList<>();
-        int rows = state.length;
-        for(int i = 0; i < rows; i++) {
+    
+    private void appendToAnswer(char[][] board) {
+        List<String> res = new ArrayList<>();
+        
+        for(int i = 0; i < board.length; i++) {
             StringBuilder strBuilder = new StringBuilder();
-            for(int j = 0; j < rows; j++) {
-                strBuilder.append(state[i][j]);
+            for(int j = 0; j < board[i].length; j++) {
+                strBuilder.append(board[i][j]);
             }
-            answer.add(strBuilder.toString());
+            res.add(new String(strBuilder));
         }
-        answers.add(answer);
+        
+        answerList.add(res);
     }
-    
-    
-    
-    
 }
