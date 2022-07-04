f09548309756076982dc8787de44f8daca4d5a88
brwnsugr <eunho@tumblbug.com>
2022-07-04 16:17:42 +0900

    Time: 14 ms (54.61%), Space: 53.6 MB (32.49%) - LeetHub

--- 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.java
+++ 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.java
@@ -1,32 +1,23 @@
 class Solution {
     public int minFlipsMonoIncr(String s) {
-        int n = s.length();
-        int[] p = new int[n+1];
-        p[0] = 0;
-        for(int i = 0; i < n; i++) {
-            int temp = s.charAt(i) == '1' ? 1 : 0;
-            p[i+1] = p[i] + temp;
-        }
-
-        /*
-        check all the elements 0 or 1
-        */
-        if(p[n] == n || p[n] == 0) return 0;
+        int len = s.length();
+        int[] prefixSums = new int[len + 1];
         int answer = Integer.MAX_VALUE;
-        //[0,1,2,2,3,4] "00110"
-        //left part len = 0, right len = n
-        // answer = Math.min(answer, n - p[n-1]);
         
-        for(int i = 0; i <= n; i++) {
-            //left number of '1's
-            int leftZeros = p[i]; // [0^,1,2,2,3,4] lz = 0, [0,1,2,2,3,4] rO = 5 - 0-1 - (4-0)
-            
-            //right number of '0's
-            int rightOnes = (n - i) - (p[n] - p[i]);
-            answer = Math.min(answer, (leftZeros + rightOnes));
+        for(int i = 0; i < len; i++) {
+            if(s.charAt(i) == '1') 
+                prefixSums[i+1] = prefixSums[i] + 1;
+            else prefixSums[i+1] = prefixSums[i];
         }
         
-        //"00110"
+        if(prefixSums[len] == len || prefixSums[len] == 0) return 0;
+        
+        for(int i = 0; i <= len; i++) {
+            int leftZeros = prefixSums[i];
+            int rightOnes = (len - i) - (prefixSums[len] - prefixSums[i]);
+            answer = Math.min(answer, leftZeros + rightOnes);
+        }
+        
         return answer;
     }
 }
