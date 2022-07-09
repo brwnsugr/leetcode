54d0f6eb8bcc1a7929b867031840f5f0546dcdfa
brwnsugr <eunho@tumblbug.com>
2022-07-09 11:43:54 +0900

    Time: 73 ms (65.83%), Space: 54.5 MB (50.02%) - LeetHub

--- 159-longest-substring-with-at-most-two-distinct-characters/159-longest-substring-with-at-most-two-distinct-characters.java
+++ 159-longest-substring-with-at-most-two-distinct-characters/159-longest-substring-with-at-most-two-distinct-characters.java
@@ -3,26 +3,20 @@ class Solution {
         
         int l = 0;
         int r = 0;
-        int[] charCounts = new int[128];
+
+        Map<Character, Integer> countMap = new HashMap<>();
         int maxLength = 0;
         while(r < s.length()) {
-            charCounts[s.charAt(r) - '0']++;
-            while(l < r && getFormedCount(charCounts) > 2) {
-                charCounts[s.charAt(l++)-'0']--;
+            countMap.put(s.charAt(r), countMap.getOrDefault(s.charAt(r), 0) + 1);
+            while(l < r && countMap.size() > 2) {
+                countMap.put(s.charAt(l), countMap.get(s.charAt(l)) - 1);
+                if(countMap.get(s.charAt(l)) == 0) countMap.remove(s.charAt(l));
+                l++;
             }
             maxLength = Math.max(maxLength, r - l + 1);
             r++;
         } 
         return maxLength;
-    }
-    
-    private int getFormedCount(int[] charCounts) {
-        int count = 0;
-        for(int charCount : charCounts) {
-            if(charCount > 0) count++;
-        }
-        return count;
-    }
-    
+    }    
     
 }
