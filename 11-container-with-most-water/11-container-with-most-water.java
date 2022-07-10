28bf6a3c8df0de91c2373a31aeb2495697401689
brwnsugr <eunho@tumblbug.com>
2022-07-10 10:41:34 +0900

    Time: 4 ms (82.60%), Space: 80.9 MB (41.95%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.java
+++ 11-container-with-most-water/11-container-with-most-water.java
@@ -3,26 +3,21 @@ class Solution {
         int answer = 0;
         int l = 0;
         int r = height.length - 1;
-        
-        // 
-        
+
         while(l < r) {
             int tempHeight = Math.min(height[l], height[r]);
             int tempArea = tempHeight * (r-l);
             
             answer = Math.max(answer, tempArea);
             
-            if(height[l] > height[r]) {
-                r--;
+            if(height[l] < height[r]) {
+                l++;
             }
             
             else {
-                l++;
+                r--;
             }
         }
-        
-        //[1^,8,6,2,5,4,8,3,7^]  
-        
         return answer;
     }
 }
