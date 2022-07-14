c20868d3020e3c2e69d007a39fcad4d112084fea
brwnsugr <eunho@tumblbug.com>
2022-07-15 00:10:57 +0900

    Time: 6 ms (21.37%), Space: 44.1 MB (68.25%) - LeetHub

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -16,24 +16,8 @@
 class Solution {
     public TreeNode buildTree(int[] inorder, int[] postorder) {
         if(postorder.length == 0) return null;
-        TreeNode root = new TreeNode(postorder[postorder.length - 1]);
-        
+        TreeNode root = new TreeNode(postorder[postorder.length - 1]);        
         if(postorder.length == 1) return root;
-        
-        //find pivot idx
-//         int pivotVal = postorder[postorder.length - 1];
-//         int pivotIdx = 0;
-//         for(int i = 0; i < inorder.length; i++) {
-//             if(inorder[i] == pivotVal) {
-//                 pivotIdx = i;
-//                 break;
-//             }
-//         }
-        
-//         root.left = buildTree(Arrays.copyOfRange(inorder, 0, pivotIdx), Arrays.copyOfRange(postorder, 0, pivotIdx));
-//         root.right = buildTree(Arrays.copyOfRange(inorder, pivotIdx+1, inorder.length), Arrays.copyOfRange(postorder, pivotIdx, postorder.length - 1));
-        
-        
         return recursiveBuild(0, inorder.length-1, 0, postorder.length - 1, inorder, postorder);
     }
     
@@ -41,10 +25,10 @@ class Solution {
     private TreeNode recursiveBuild(int inStart, int inEnd, int pStart, int pEnd, int[] inorder, int[] postorder) {
         if(pEnd < 0 || inStart > inEnd) return null; 
         TreeNode root = new TreeNode(postorder[pEnd]);
-        
-        // if(pStart == pEnd) return root;
+
         int pivotVal = postorder[pEnd];
         int pivotIdx = 0;
+
         for(int i = inStart; i <= inEnd; i++) {
             if(inorder[i] == pivotVal) {
                 pivotIdx = i;
@@ -52,9 +36,8 @@ class Solution {
             }
         }
         
-        root.left = recursiveBuild(inStart, pivotIdx-1, 0, pEnd - 1 - inEnd + pivotIdx, inorder, postorder);
-        root.right = recursiveBuild(pivotIdx+1, inEnd, pStart + pivotIdx, pEnd - 1, inorder, postorder);
-        
+        root.left = recursiveBuild(inStart, pivotIdx - 1, 0, pEnd - 1 - inEnd + pivotIdx, inorder, postorder);
+        root.right = recursiveBuild(pivotIdx + 1, inEnd, pStart + pivotIdx, pEnd - 1, inorder, postorder);
         return root;
     }
 }
