5dfee52f8b394d9f164f4144854e152b5e6193cd
brwnsugr <eunho@tumblbug.com>
2022-07-14 22:41:07 +0900

    Time: 5 ms (34.81%), Space: 42.2 MB (85.78%) - LeetHub

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -21,18 +21,39 @@ class Solution {
         if(postorder.length == 1) return root;
         
         //find pivot idx
-        int pivotVal = postorder[postorder.length - 1];
+//         int pivotVal = postorder[postorder.length - 1];
+//         int pivotIdx = 0;
+//         for(int i = 0; i < inorder.length; i++) {
+//             if(inorder[i] == pivotVal) {
+//                 pivotIdx = i;
+//                 break;
+//             }
+//         }
+        
+//         root.left = buildTree(Arrays.copyOfRange(inorder, 0, pivotIdx), Arrays.copyOfRange(postorder, 0, pivotIdx));
+//         root.right = buildTree(Arrays.copyOfRange(inorder, pivotIdx+1, inorder.length), Arrays.copyOfRange(postorder, pivotIdx, postorder.length - 1));
+        
+        
+        return recursiveBuild(0, inorder.length-1, 0, postorder.length - 1, inorder, postorder);
+    }
+    
+    
+    private TreeNode recursiveBuild(int inStart, int inEnd, int pStart, int pEnd, int[] inorder, int[] postorder) {
+        if(pEnd < 0 || inStart > inEnd) return null; 
+        TreeNode root = new TreeNode(postorder[pEnd]);
+        
+        // if(pStart == pEnd) return root;
+        int pivotVal = postorder[pEnd];
         int pivotIdx = 0;
-        for(int i = 0; i < inorder.length; i++) {
+        for(int i = inStart; i <= inEnd; i++) {
             if(inorder[i] == pivotVal) {
                 pivotIdx = i;
                 break;
             }
         }
         
-        root.left = buildTree(Arrays.copyOfRange(inorder, 0, pivotIdx), Arrays.copyOfRange(postorder, 0, pivotIdx));
-        root.right = buildTree(Arrays.copyOfRange(inorder, pivotIdx+1, inorder.length), Arrays.copyOfRange(postorder, pivotIdx, postorder.length - 1));
-        
+        root.left = recursiveBuild(inStart, pivotIdx-1, 0, pEnd - 1 - inEnd + pivotIdx, inorder, postorder);
+        root.right = recursiveBuild(pivotIdx+1, inEnd, pStart + pivotIdx, pEnd - 1, inorder, postorder);
         
         return root;
     }
