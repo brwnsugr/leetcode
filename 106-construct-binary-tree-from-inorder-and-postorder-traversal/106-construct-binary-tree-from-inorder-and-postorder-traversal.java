5730e20da9ff37629aca57184a5d98f11747f5b4
brwnsugr <eunho@tumblbug.com>
2022-07-14 09:46:07 +0900

    Time: 21 ms (5.15%), Space: 91.7 MB (5.04%) - LeetHub

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -15,21 +15,23 @@
  */
 class Solution {
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-        
-        // 
-        if(inorder.length == 0) return null;
+        if(postorder.length == 0) return null;
         TreeNode root = new TreeNode(postorder[postorder.length - 1]);
+        
         if(postorder.length == 1) return root;
+        
+        //find pivot idx
+        int pivotVal = postorder[postorder.length - 1];
         int pivotIdx = 0;
-        for(int idx = 0; idx < inorder.length; idx++) {
-            if(inorder[idx] == root.val) {
-                pivotIdx = idx;
+        for(int i = 0; i < inorder.length; i++) {
+            if(inorder[i] == pivotVal) {
+                pivotIdx = i;
                 break;
             }
         }
         
         root.left = buildTree(Arrays.copyOfRange(inorder, 0, pivotIdx), Arrays.copyOfRange(postorder, 0, pivotIdx));
-        root.right = buildTree(Arrays.copyOfRange(inorder, pivotIdx + 1, inorder.length), Arrays.copyOfRange(postorder, pivotIdx, postorder.length - 1));
+        root.right = buildTree(Arrays.copyOfRange(inorder, pivotIdx+1, inorder.length), Arrays.copyOfRange(postorder, pivotIdx, postorder.length - 1));
         
         
         return root;
