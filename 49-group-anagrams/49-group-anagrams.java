25ad03d5845ff2dd809cb0bb5e0ee37304a169a6
brwnsugr <eunho@tumblbug.com>
2022-07-17 15:35:54 +0900

    Time: 39 ms (18.11%), Space: 58.3 MB (24.30%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -1,23 +1,26 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        
-        
-        // making key as 2 ways : 
-        // 1. sort it. (abc, cba) -> abc(sorted!) and key itself
-        //
         if(strs.length == 0) return new ArrayList<>();
-        
         Map<String, List<String>> anaMap = new HashMap<>();
         
+        int[] countArr = new int[26];
+        
         for(String s : strs) {
-            char[] chArr = s.toCharArray();
-            Arrays.sort(chArr);
-            String key = String.valueOf(chArr);
+            Arrays.fill(countArr, 0);
+            for(char c : s.toCharArray()) countArr[c-'a']++;
+            
+            StringBuilder strBuilder = new StringBuilder();
+            
+            for(int i = 0; i < 26; i++) {
+                strBuilder.append('#');
+                strBuilder.append(countArr[i]);
+            }
+            
+            String key = strBuilder.toString();
             if(!anaMap.containsKey(key)) anaMap.put(key, new ArrayList<>());
             anaMap.get(key).add(s);
         }
         
         return new ArrayList<>(anaMap.values());
-        
     }
 }
