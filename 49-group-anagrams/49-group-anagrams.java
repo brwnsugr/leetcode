f8532fb04520db4404277f2223622d2ac95e8b39
brwnsugr <eunho@tumblbug.com>
2022-07-17 15:01:13 +0900

    Time: 13 ms (60.62%), Space: 55.6 MB (67.71%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -1,34 +1,23 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> answers = new ArrayList<>();
+        
+        
+        // making key as 2 ways : 
+        // 1. sort it. (abc, cba) -> abc(sorted!) and key itself
+        //
+        if(strs.length == 0) return new ArrayList<>();
+        
         Map<String, List<String>> anaMap = new HashMap<>();
         
-        for(String word : strs) {
-            String key = makeKey(word);
+        for(String s : strs) {
+            char[] chArr = s.toCharArray();
+            Arrays.sort(chArr);
+            String key = String.valueOf(chArr);
             if(!anaMap.containsKey(key)) anaMap.put(key, new ArrayList<>());
-            anaMap.get(key).add(word);
+            anaMap.get(key).add(s);
         }
         
-        for(Map.Entry<String, List<String>> entry : anaMap.entrySet()) {
-            answers.add(entry.getValue()); 
-        }
+        return new ArrayList<>(anaMap.values());
         
-        return answers;
-    }
-    
-    
-    private String makeKey(String word) {
-        int[] counts = new int[26];
-        for(char c : word.toCharArray()) {
-            counts[c-'a']++;
-        }
-        StringBuilder strBuilder = new StringBuilder();
-        for(int i = 0; i < counts.length; i++) {
-            if(counts[i] > 0) {
-                char c = (char) (i + 'a');
-                strBuilder.append(c).append(counts[i]);
-            }
-        }
-        return new String(strBuilder);
     }
 }
