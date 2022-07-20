39d58ee5b3b9199f9ea6c0293e5fd6a41276053f
brwnsugr <eunho@tumblbug.com>
2022-07-20 21:25:32 +0900

    Time: 1 ms (97.51%), Space: 43 MB (81.49%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.java
+++ 22-generate-parentheses/22-generate-parentheses.java
@@ -1,42 +1,31 @@
 class Solution {
-  private List<String> res = new ArrayList<>();
-  public List<String> generateParenthesis(int n) {
-    Stack<Character> s = new Stack<>();
-    Map<Character, Integer> m = new HashMap<>();
-    m.put('(', 1);
-    m.put(')', 0);
-    s.add('(');
-    dfs(m, "(", s, n);
-    return res;
-  }
-
-  private void dfs(Map<Character, Integer> m,  String paren, Stack<Character> s, int n) {
-    if(s.isEmpty() && m.get('(') == n && m.get(')') == n) {
-      res.add(String.copyValueOf(paren.toCharArray()));
-      return;
-    } else {
-      if(s.isEmpty()) {
-        s.add('(');
-        m.put('(', m.get('(')+1);
-        dfs(m, paren + "(", s,n);
-        s.pop();
-        m.put('(', m.get('(')-1);
-      }
-      if(!s.isEmpty() && s.peek() == '(' && m.get(')') < n) {
-        s.pop();
-        m.put(')', m.get(')')+1);
-        dfs(m, paren + ")", s, n);
-        m.put(')', m.get(')')-1);
-        s.add('(');
-      }
-      if(!s.isEmpty() && s.peek() == '(' && m.get('(') < n) {
-        s.add('(');
-        m.put('(', m.get('(')+1);
-        dfs(m, paren + "(", s, n);
-        m.put('(', m.get('(')-1);
-        s.pop();
-      }
+    
+    private List<String> answer = new ArrayList<>();
+    private int pairs;
+    
+    public List<String> generateParenthesis(int n) {
+        pairs = n;
+        recursiveGenerate(0, 0, new StringBuilder());
+        return answer;
     }
-    return;
-  }
+    
+    
+    private void recursiveGenerate(int openCount, int closeCount, StringBuilder strBuilder) {
+        if(openCount == pairs && closeCount == pairs) {
+            answer.add(new String(strBuilder));
+            return;
+        }
+        
+        if(openCount >= closeCount && openCount < pairs) {
+            strBuilder.append('(');
+            recursiveGenerate(openCount+1, closeCount, strBuilder);
+            strBuilder.deleteCharAt(strBuilder.length() - 1);
+        }
+        
+        if(openCount > closeCount) {
+            strBuilder.append(')');
+            recursiveGenerate(openCount, closeCount+1, strBuilder);
+            strBuilder.deleteCharAt(strBuilder.length() - 1);
+        }
+    }
 }
