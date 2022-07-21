38e5382c30af4cee750b746ea38c626e6380d3da
brwnsugr <eunho@tumblbug.com>
2022-07-21 22:53:18 +0900

    Time: 98 ms (9.75%), Space: 66.5 MB (14.58%) - LeetHub

--- 77-combinations/77-combinations.java
+++ 77-combinations/77-combinations.java
@@ -1,51 +1,24 @@
 class Solution {
-    
-    List<List<Integer>> answers = new ArrayList<>();
-    private int targetK;
-    
-    
+
+    List<List<Integer>> answer = new ArrayList<>();
     public List<List<Integer>> combine(int n, int k) {
-        // 
-        // 4P2 = 4C2 * 2!  
-        // 1,2,3,     x-> (2 ->(3 or x) or x -> ) ->
-        //            1 -> (2 or x)
-        // 
-        //   1,2
-        // n = 4,, k = 2
-        // -> 1^,2^,3,4          pickedCount = 0, 
-        // [1,2]              pickedCount = 2
-        // O(2^N) -> 
-        //
-        targetK = k;
-        LinkedList<Integer> paths = new LinkedList<>();
-        comb(n, 1, 0, paths);
-        return answers;
+        backTrack(n, k, 1, 0, new LinkedList<>());
+        return answer;
     }
     
-    
-    private void comb(int n, int currNumber, int pickedCount, LinkedList<Integer> paths) {
-        
-        if(pickedCount == targetK) {
-            answers.add(new ArrayList<>(paths));
+    private void backTrack(int n, int k, int curr, int count, List list) {
+        if(count == k) {
+            answer.add(new ArrayList<>(list));
             return;
         }
-        
-        if(currNumber > n) {
-            return;
+        else {
+            for(int i = curr; i <= n; i++) {
+                list.add(i);
+                backTrack(n, k, i+1, count+1, list);
+                list.remove(list.size() - 1);
+            }    
         }
-        
-        
-        //1. 현재꺼를 pick
-        paths.addLast(currNumber); // n = 4, k = 2.  [3, 4] 2 paths = [1]
-        comb(n, currNumber+1, pickedCount+1, paths);
-        paths.removeLast();
-        
-        //2. 현재꺼를 pick X
-        comb(n, currNumber+1, pickedCount, paths);
-        
-        
     }
-    
     
     
 }
