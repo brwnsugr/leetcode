0cc36d6ae49b59fcac9542549b8cfd5a2ed87af6
brwnsugr <eunho@tumblbug.com>
2022-07-21 22:55:46 +0900

    Time: 43 ms (36.58%), Space: 67.4 MB (13.24%) - LeetHub

--- 77-combinations/77-combinations.java
+++ 77-combinations/77-combinations.java
@@ -6,16 +6,16 @@ class Solution {
         return answer;
     }
     
-    private void backTrack(int n, int k, int curr, int count, List list) {
+    private void backTrack(int n, int k, int curr, int count, LinkedList list) {
         if(count == k) {
             answer.add(new ArrayList<>(list));
             return;
         }
         else {
             for(int i = curr; i <= n; i++) {
-                list.add(i);
+                list.addLast(i);
                 backTrack(n, k, i+1, count+1, list);
-                list.remove(list.size() - 1);
+                list.removeLast();
             }    
         }
     }
