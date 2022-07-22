6883521804d2b66d2c5c752c5051c3dc6ba3144a
brwnsugr <eunho@tumblbug.com>
2022-07-22 20:44:51 +0900

    Time: 1 ms (95.39%), Space: 42.5 MB (72.45%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -1,38 +1,40 @@
 class Solution {
     
-    private List<String> answers;
-    private String phoneDigits;
-    private static Map<Integer, char[]> dialMap = new HashMap<>(){{
-        put(2, new char[]{'a','b','c'});
-        put(3, new char[]{'d','e','f'});
-        put(4, new char[]{'g','h','i'});
-        put(5, new char[]{'j','k','l'});
-        put(6, new char[]{'m','n','o'});
-        put(7, new char[]{'p','q','r','s'});
-        put(8, new char[]{'t','u','v'});
-        put(9, new char[]{'w','x','y','z'});}};
+    private char[][] numberBook = new char[][]{
+        {' '},
+        {'a', 'b', 'c'},
+        {'d', 'e', 'f'},
+        {'g', 'h', 'i'},
+        {'j', 'k', 'l'},
+        {'m', 'n', 'o'},
+        {'p', 'q', 'r', 's'},
+        {'t', 'u', 'v'},
+        {'w', 'x', 'y', 'z'},
+        {'a', 'b', 'c'},
+    };
     
+    private List<String> answer = new ArrayList<>();
     
     public List<String> letterCombinations(String digits) {
-        //2: [abc], 3: [def]
-        answers = new ArrayList<>();
-        phoneDigits = digits;
-        if(digits.length() == 0) return answers;
-        recursive(0, new StringBuilder());
-        return answers;
+        if(digits.equals("")) return new ArrayList<>();
+        recursion(0, digits, new StringBuilder());
+        return answer;
     }
     
-    private void recursive(int idx, StringBuilder strBuilder) {
-        if(strBuilder.length() == phoneDigits.length()) {
-            answers.add(new String(strBuilder));
+    private void recursion(int currIdx, String digits, StringBuilder strBuilder) {
+        if(currIdx == digits.length()) {
+            answer.add(new String(strBuilder));
             return;
         }
-        int currNum = phoneDigits.charAt(idx) - '0';
-        char[] chars = dialMap.get(currNum);
-        for(char c : chars) {
-            strBuilder.append(c);
-            recursive(idx+1, strBuilder);
-            strBuilder.deleteCharAt(strBuilder.length()-1);
+        
+        else {
+            int currNumber = (int) digits.charAt(currIdx) - '0';
+            char[] currDials = numberBook[currNumber-1];
+            for(char c : currDials) {
+                strBuilder.append(c);
+                recursion(currIdx+1, digits, strBuilder);
+                strBuilder.deleteCharAt(strBuilder.length() - 1);
+            }
         }
     }
 }
