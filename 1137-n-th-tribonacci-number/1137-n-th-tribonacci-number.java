d1ba807dd1953b8512921a5fd625dd6867ab78a5
brwnsugr <eunho@tumblbug.com>
2022-07-26 19:03:30 +0900

    Time: 0 ms (100.00%), Space: 40.8 MB (48.07%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -1,14 +1,15 @@
 class Solution {
     public int tribonacci(int n) {
-        if(n==0) return 0;
-        if(n == 1 || n == 2) return 1;
-        int cache[] = new int[n+1];
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        else if(n == 2) return 1;
+        int[] cache = new int[n+1];
         cache[0] = 0; cache[1] = 1; cache[2] = 1;
         
         for(int i = 3; i <= n; i++) {
             cache[i] = cache[i-1] + cache[i-2] + cache[i-3];
         }
+        
         return cache[n];
     }
-    
 }
