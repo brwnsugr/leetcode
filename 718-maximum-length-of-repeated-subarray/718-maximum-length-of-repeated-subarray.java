b6fccae5bd909c9d3bc448a11f3c932178d8733f
brwnsugr <eunho@tumblbug.com>
2022-07-26 18:39:05 +0900

    Time: 2451 ms (5.03%), Space: 145.1 MB (5.11%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
@@ -1,18 +1,28 @@
 class Solution {
-    public int findLength(int[] nums1, int[] nums2) {
-        int[][] dp = new int[nums1.length + 1][nums2.length + 1];
-        int answer = Integer.MIN_VALUE;
-        
-        for(int i = nums1.length - 1; i >= 0; i--) {
-            for(int j = nums2.length - 1; j >= 0; j--) {
-                if(nums1[i] == nums2[j]) {
-                    dp[i][j] = dp[i+1][j+1] + 1;
-                }
-                
-                answer = Math.max(dp[i][j], answer);
-            }
+    public int findLength(int[] A, int[] B) {
+        int lo = 0, hi = Math.min(A.length, B.length);
+        while (lo < hi) {
+            int mi = lo + (hi - lo + 1) / 2;
+            if (checkLength(mi, A, B)) lo = mi;
+            else hi = mi - 1;
         }
-        
-        return answer;
+        return lo;
+    }
+
+    private boolean checkLength(int x, int[] A, int[] B) {
+        if (x == 0) return true;
+        else if (x > Math.min(A.length, B.length)) return false;
+        else {
+            Set<String> seen = new HashSet<>();
+            for (int i = 0; i + x <= A.length; i++) {
+                seen.add(Arrays.toString(Arrays.copyOfRange(A, i, i + x)));
+            }
+            for (int i = 0; i + x <= B.length; i++) {
+                if (seen.contains(Arrays.toString(Arrays.copyOfRange(B, i, i + x)))) {
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 }
