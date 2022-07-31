42788542f33dfe07297151eea7431f9a1106442d
brwnsugr <eunho@tumblbug.com>
2022-07-31 11:08:14 +0900

    Time: 2 ms (84.19%), Space: 44.9 MB (39.68%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.java
+++ 113-path-sum-ii/113-path-sum-ii.java
@@ -15,31 +15,37 @@
  */
 class Solution {
     
-    private List<List<Integer>> answers = new ArrayList<>();
+    private List<List<Integer>> answer = new ArrayList<>();
     private int target;
-    
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
-        if(root == null) return new ArrayList<>();
-        LinkedList<Integer> path = new LinkedList<>();
         target = targetSum;
-        recursive(root, path, 0);
-        return answers;
+        if(root == null) return new ArrayList<>();
+        List<Integer> trace = new ArrayList<>();
+        trace.add(root.val);
+        preOrder(root, trace, root.val);
+        
+        return answer;
     }
     
-    private void recursive(TreeNode root, LinkedList<Integer> path, int tempSum) {
-        //meet leaf node
+    private void preOrder(TreeNode root, List<Integer> trace, int tempSum) {
         if(root == null) return;
-        path.add(root.val);
-        tempSum += root.val;
-        if(root.left == null && root.right == null && tempSum == target) {
-            answers.add(new ArrayList<>(path));
+        if(root.left == null && root.right == null) {
+            if(tempSum == target) {
+                answer.add(new ArrayList<>(trace));
+            }
+            return;
         }
-        // else {
-        recursive(root.left, path, tempSum);
-        recursive(root.right, path, tempSum);
-        // }
-        path.removeLast();
-        tempSum -= root.val;
-            
+        else {
+            if(root.left != null) {
+                trace.add(root.left.val);
+                preOrder(root.left, trace, tempSum + root.left.val);
+                trace.remove(trace.size() - 1);
+            }
+            if(root.right != null) {
+                trace.add(root.right.val);
+                preOrder(root.right, trace, tempSum + root.right.val);
+                trace.remove(trace.size() - 1);
+            }     
+        }
     }
 }
