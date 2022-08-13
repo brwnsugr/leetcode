af624b43338b44d48d8c942e4c5c8c857e372cf9
brwnsugr <eunho@tumblbug.com>
2022-08-14 01:52:17 +0900

    Time: 180 ms (14.74%), Space: 117.5 MB (10.12%) - LeetHub

--- 472-concatenated-words/472-concatenated-words.java
+++ 472-concatenated-words/472-concatenated-words.java
@@ -1,12 +1,14 @@
 class Solution {
+    
+    private Set<String> cache = new HashSet<>();
     public List<String> findAllConcatenatedWordsInADict(String[] words) {
-       
         Set<String> wordDict = new HashSet<>();
         List<String> answer = new ArrayList<>();
         for(String word : words) wordDict.add(word);
         
         for(String word : words) {
             if(isValid(word, wordDict)) answer.add(word);
+            cache = new HashSet<>();
         }
         
         return answer;
@@ -14,10 +16,12 @@ class Solution {
     
     
     private boolean isValid(String word, Set<String> wordDict) {
+        if(cache.contains(word)) return true;
         for(int i = 1; i < word.length(); i++) {
             String left = word.substring(0, i);
             String right = word.substring(i);
             if(wordDict.contains(left) && (wordDict.contains(right) || isValid(right, wordDict))) {
+                cache.add(right);
                 return true;
             }
         }
