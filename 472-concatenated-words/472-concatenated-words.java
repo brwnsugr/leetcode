967c21488e9639916e6aed21ba23659bb406423f
brwnsugr <eunho@tumblbug.com>
2022-08-14 01:47:30 +0900

    Time: 75 ms (85.22%), Space: 48.1 MB (91.66%) - LeetHub

--- 472-concatenated-words/472-concatenated-words.java
+++ 472-concatenated-words/472-concatenated-words.java
@@ -1,35 +1,26 @@
 class Solution {
     public List<String> findAllConcatenatedWordsInADict(String[] words) {
-        if(words.length == 1) return new ArrayList<>();
-        List<String> answers = new ArrayList<>();
+       
         Set<String> wordDict = new HashSet<>();
-        
+        List<String> answer = new ArrayList<>();
         for(String word : words) wordDict.add(word);
         
         for(String word : words) {
-            if(isValid(word, wordDict)) answers.add(word);
+            if(isValid(word, wordDict)) answer.add(word);
         }
         
-        return answers;
+        return answer;
     }
     
     
     private boolean isValid(String word, Set<String> wordDict) {
-        boolean[] dp = new boolean[word.length() + 1];
-        
-        dp[0] = true;
-        int count = 0;
-        for(int i = 1; i <= word.length(); i++) {
-            for(int j = 0; j < i; j++) {
-                
-                if(dp[j] && wordDict.contains(word.substring(j, i)) && !word.substring(j,i).equals(word)) {
-                    
-                    dp[i] = true;
-                    count++;
-                }
+        for(int i = 1; i < word.length(); i++) {
+            String left = word.substring(0, i);
+            String right = word.substring(i);
+            if(wordDict.contains(left) && (wordDict.contains(right) || isValid(right, wordDict))) {
+                return true;
             }
         }
-
-        return count > 1 ? dp[word.length()] : false;    
+        return false;
     }
 }
