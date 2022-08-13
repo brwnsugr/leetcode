2f715685e2ee70c1b7f0cf2d48764f21c6d8411b
brwnsugr <eunho@tumblbug.com>
2022-08-13 09:45:18 +0900

    Time: 24 ms (76.38%), Space: 46.4 MB (26.80%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
@@ -1,34 +1,32 @@
 class Solution {
     public long subArrayRanges(int[] nums) {
-        
-        // [1,3,6]
-        //arr = [1,3,3]
-        //minFromI = [1,3,3]
-        //maxTilI = [1,3,3]
-        
-        Stack<Integer> s = new Stack<>();
-        int numLen = nums.length;
+        Stack<Integer> decStack = new Stack<>();
+        Stack<Integer> incStack = new Stack<>();
         long answer = 0;
-        for(int i = 0; i <= numLen; i++) {
-            while(!s.isEmpty() && (i == numLen || nums[s.peek()] < nums[i])) {
-                int top = s.pop();
-                int l = s.isEmpty() ? top + 1 : top - s.peek();
-                int r = i - top;
-                answer += (long) nums[top] * r * l;
+        
+        for(int i = 0; i <= nums.length; i++) {
+            
+            while(!decStack.isEmpty() && (i == nums.length || nums[decStack.peek()] < nums[i])) {
+                int currIdx = decStack.pop();
+                int leftBound = decStack.isEmpty() ? -1 : decStack.peek();
+                int rightBound = i;
+                answer += (long) ((rightBound - currIdx) * (currIdx - leftBound)) * nums[currIdx];
             }
-            s.add(i);
-        }
-        s.clear();
-        for(int i = 0; i <= numLen; i++) {
-            while(!s.isEmpty() && (i == numLen || nums[s.peek()] > nums[i])) {
-                int top = s.pop();
-                int l = s.isEmpty() ? top + 1 : top - s.peek();
-                int r = i - top;
-                answer -= (long) nums[top] * r * l;
+            
+            
+            while(!incStack.isEmpty() && (i == nums.length || nums[incStack.peek()] > nums[i])) {
+                int currIdx = incStack.pop();
+                int leftBound = incStack.isEmpty() ? -1 : incStack.peek();
+                int rightBound = i;
+                answer -= (long) ((rightBound - currIdx) * (currIdx - leftBound)) * nums[currIdx];
             }
-            s.add(i);
+            
+            
+            decStack.add(i);
+            incStack.add(i);
         }
         
         return answer;
+        
     }
 }
