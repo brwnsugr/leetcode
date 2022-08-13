14e028d00bd6796c26e80267fb491ab2c04ecd9c
brwnsugr <eunho@tumblbug.com>
2022-08-13 16:19:24 +0900

    Time: 16 ms (19.39%), Space: 49.4 MB (31.93%) - LeetHub

--- 937-reorder-data-in-log-files/937-reorder-data-in-log-files.java
+++ 937-reorder-data-in-log-files/937-reorder-data-in-log-files.java
@@ -1,27 +1,25 @@
 class Solution {
     public String[] reorderLogFiles(String[] logs) {
-    
+        
+        
         Arrays.sort(logs, new Comparator<String>(){
-           
             @Override
-            public int compare(String t1, String t2) {
+            public int compare(String p1, String p2) {
+                String[] p1Str = p1.split(" ", 2);
+                String[] p2Str = p2.split(" ", 2);
                 
-                String[] p1 = t1.split(" ", 2);
-                String[] p2 = t2.split(" ", 2);
-                boolean p1Digit = Character.isDigit(p1[1].charAt(0));
-                boolean p2Digit = Character.isDigit(p2[1].charAt(0));
+                boolean p1Digit = Character.isDigit(p1Str[1].charAt(0));
+                boolean p2Digit = Character.isDigit(p2Str[1].charAt(0));
                 
                 if(!p1Digit && p2Digit) return -1;
                 else if(p1Digit && !p2Digit) return 1;
-                else if(!p1Digit && !p2Digit){
-                    if(p1[1].equals(p2[1])) {
-                        return p1[0].compareTo(p2[0]);
-                    }
-                    return p1[1].compareTo(p2[1]);
+                else if(!p1Digit && !p2Digit) {
+                    if(p1Str[1].equals(p2Str[1])) {
+                        return p1Str[0].compareTo(p2Str[0]);
+                    } 
+                    else return p1Str[1].compareTo(p2Str[1]);
                 }
-                else {
-                    return 0;
-                }
+                else return 0;
             }
         });
         
