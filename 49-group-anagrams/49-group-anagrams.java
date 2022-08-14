e137b3b5d3b77e11978a955135c976205a67dfd2
brwnsugr <eunho@tumblbug.com>
2022-08-14 15:01:33 +0900

    Time: 21 ms (30.73%), Space: 58.5 MB (23.67%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -1,26 +1,37 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        if(strs.length == 0) return new ArrayList<>();
         Map<String, List<String>> anaMap = new HashMap<>();
-        
-        int[] countArr = new int[26];
-        
-        for(String s : strs) {
-            Arrays.fill(countArr, 0);
-            for(char c : s.toCharArray()) countArr[c-'a']++;
+        List<List<String>> answer = new ArrayList<>();
+        for(String str : strs) {
+            String key = buildKey(str);
             
-            StringBuilder strBuilder = new StringBuilder();
-            
-            for(int i = 0; i < 26; i++) {
-                strBuilder.append('#');
-                strBuilder.append(countArr[i]);
-            }
-            
-            String key = strBuilder.toString();
             if(!anaMap.containsKey(key)) anaMap.put(key, new ArrayList<>());
-            anaMap.get(key).add(s);
+            anaMap.get(key).add(str);
         }
         
-        return new ArrayList<>(anaMap.values());
+        for(Map.Entry<String, List<String>> entry : anaMap.entrySet()) {
+            answer.add(entry.getValue());
+        }
+        return answer;
+    }
+    
+    private String buildKey(String input) {
+        int[] counts = new int[26];
+        
+        for(char c : input.toCharArray()) {
+            counts[c-'a']++;
+        }
+        
+        StringBuilder strBuilder = new StringBuilder();
+        
+        for(int i = 0; i < 26; i++) {
+            if(counts[i] > 0) {
+                char c = (char)(i + 'a');
+                strBuilder.append(c);
+                strBuilder.append(counts[i]);
+            }
+        }
+        
+        return new String(strBuilder);
     }
 }
