869c407665de875dffdd9559c2a964490ea58b2f
brwnsugr <eunho@tumblbug.com>
2022-08-14 21:24:59 +0900

    Time: 22 ms (7.05%), Space: 44.6 MB (79.70%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.java
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.java
@@ -1,14 +1,20 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
+        Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
         
-        Arrays.sort(intervals, (a,b) -> a[0] - b[0]); 
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int rooms = 0;
+        
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
         
         for(int i = 0; i < intervals.length; i++) {
-            pq.add(intervals[i][1]);
-            int minEndTime = pq.peek().intValue();
-            if(intervals[i][0] >= minEndTime) pq.remove();
+            while(!pq.isEmpty() && intervals[i][0] >= pq.peek()[1]) {
+                pq.remove();
+            }
+            
+            pq.add(intervals[i]);
+            rooms = Math.max(rooms, pq.size());
         }
-        return pq.size();
+        
+        return rooms;
     }
 }
