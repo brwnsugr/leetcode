fe8702700b5051b816137f7101407c46c177a0b9
brwnsugr <eunho@tumblbug.com>
2022-08-15 00:29:19 +0900

    Time: 13 ms (59.63%), Space: 48.7 MB (64.32%) - LeetHub

--- 2262-total-appeal-of-a-string/2262-total-appeal-of-a-string.java
+++ 2262-total-appeal-of-a-string/2262-total-appeal-of-a-string.java
@@ -1,24 +1,22 @@
 class Solution {
     public long appealSum(String s) {
-        int[] arr = new int[26];
+        // abc
+        // 1: a,b,c
+        // 2: ab, bc, 
+        // 3: abc
+        long answer = 0;
         
-        Arrays.fill(arr, -1);
+        int[] lastIndices = new int[26];
+        Arrays.fill(lastIndices, -1);
         
-        long currAppeals = 0;
-        long appealSum = 0;
-        char[] charArr = s.toCharArray();
-        
-        for(int i = 0; i < charArr.length; i++){
-            if(arr[charArr[i]-'a'] == -1) {
-                currAppeals = currAppeals + i + 1;
-            }
-            else{
-                currAppeals = currAppeals + (i - arr[charArr[i]-'a']);
-            }
-            arr[charArr[i]-'a'] = i;
-            appealSum += currAppeals;
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            int right = s.length() - i;
+            int left = i - lastIndices[c-'a'];
+            answer += (long)(left * right);
+            lastIndices[c-'a'] = i;
         }
         
-        return appealSum;
+        return answer;
     }
 }
