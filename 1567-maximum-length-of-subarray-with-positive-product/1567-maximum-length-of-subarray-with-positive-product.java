ce4692061c4be35acc5ecf3a229ab29578130828
brwnsugr <eunho@tumblbug.com>
2022-08-14 16:12:08 +0900

    Time: 6 ms (41.08%), Space: 83.8 MB (23.07%) - LeetHub

--- 1567-maximum-length-of-subarray-with-positive-product/1567-maximum-length-of-subarray-with-positive-product.java
+++ 1567-maximum-length-of-subarray-with-positive-product/1567-maximum-length-of-subarray-with-positive-product.java
@@ -1,23 +1,30 @@
 class Solution {
     public int getMaxLen(int[] nums) {
+        int lastNegIdx = -1;
+        int nonZeroStartIdx = -1;
         int firstNegIdx = -1;
-        int startIdx = -1;
         int negCount = 0;
         int answer = 0;
         
         for(int i = 0; i < nums.length; i++) {
-            if(nums[i] < 0) {
-                negCount++;
-                if(firstNegIdx == -1) firstNegIdx = i;
-            }
-            else if(nums[i] == 0) {
+            int curr = nums[i];
+            if(curr == 0) {
+                nonZeroStartIdx = i;
                 negCount = 0;
                 firstNegIdx = -1;
-                startIdx = i;
             }
-            int tempLength = negCount % 2 == 0 ? i - startIdx : i - firstNegIdx;
-            answer = Math.max(tempLength, answer);
+            else {
+                if(curr < 0) {
+                    negCount++;
+                    lastNegIdx = i;
+                    if(firstNegIdx == -1) firstNegIdx = i;
+                }
+
+                int tempLen = negCount % 2 == 0 ? i - nonZeroStartIdx : i - firstNegIdx;
+                answer = Math.max(tempLen, answer);
+            }
         }
+        
         return answer;
     }
 }
