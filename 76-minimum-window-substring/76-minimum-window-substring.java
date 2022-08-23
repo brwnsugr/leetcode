8c8689f247ac6de1c8dc6d4c5451c7e48d3ef238
brwnsugr <eunho@tumblbug.com>
2022-08-24 00:11:50 +0900

    Time: 24 ms (49.10%), Space: 44.6 MB (55.95%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.java
+++ 76-minimum-window-substring/76-minimum-window-substring.java
@@ -1,54 +1,45 @@
 class Solution {
     public String minWindow(String s, String t) {
-        Map<Character, Integer> originCharMap = new HashMap<>();
+        // A:0, B:0, C:0 required:3,   
         
+        // S = "ABDC"E-> "AAAA"
+        // 
+        Map<Character, Integer> tMap = new HashMap<>();
         for(char c : t.toCharArray()) {
-            if(!originCharMap.containsKey(c)) originCharMap.put(c, 0);
-            originCharMap.put(c, originCharMap.get(c) + 1);
+            tMap.put(c, tMap.getOrDefault(c, 0) + 1);
         }
         
+        int requiredCount = tMap.size();
         
-        Map<Character, Integer> windowMap = new HashMap<>();
         int r = 0;
         int l = 0;
-        int[] windowBoundary = new int[]{0,0};
-        int minWidth = Integer.MAX_VALUE;
+        int formedCount = 0;
+        int minLen = s.length() + 1;
         String res = "";
-        
         while(r < s.length()) {
-            //
-            char curr = s.charAt(r);
-            
-            if(!windowMap.containsKey(curr)) windowMap.put(curr, 0);
-            windowMap.put(curr, windowMap.get(curr) + 1);
+            char rightChar = s.charAt(r);
             boolean isFormed = false;
-            while(l <= r && formed(originCharMap, windowMap)) {
-                char leftChar = s.charAt(l);
-                windowMap.put(leftChar, windowMap.get(leftChar) - 1);
-                l++;
+            if(tMap.containsKey(rightChar)) {
+                tMap.put(rightChar, tMap.get(rightChar) - 1);
+                if(tMap.get(rightChar) == 0) formedCount++;
+            }
+            
+            while(l <= r && formedCount == requiredCount) {
                 isFormed = true;
+                char leftChar = s.charAt(l);
+                if(tMap.containsKey(leftChar)) {
+                    tMap.put(leftChar, tMap.get(leftChar) + 1);
+                    if(tMap.get(leftChar) > 0) formedCount--;
+                }
+                l++;
             }
-            
-            if(isFormed && r - l < minWidth) {
-                minWidth = r - l;
-                res = s.substring(l - 1, r + 1);
+            if(r - l + 1 < minLen && isFormed) {
+                res = s.substring(l-1, r+1); 
+                minLen = r - l + 1;
             }
-            
             r++;
         }
         
-        
-        
-        return res; 
-    }
-    
-    
-    private boolean formed(Map<Character, Integer> originMap, Map<Character, Integer> windowMap ) {
-        for(Map.Entry<Character, Integer> entry : originMap.entrySet()) {
-            if(!windowMap.containsKey(entry.getKey()) || windowMap.get(entry.getKey()) < entry.getValue()) {
-                return false;
-            }
-        }
-        return true;
+        return res;
     }
 }
