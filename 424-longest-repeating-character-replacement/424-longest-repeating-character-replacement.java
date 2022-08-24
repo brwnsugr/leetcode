ec60833490148fe4d3dbaffce3d1a24ad52e10a9
brwnsugr <eunho@tumblbug.com>
2022-08-24 19:57:36 +0900

    Time: 15 ms (47.36%), Space: 43 MB (71.17%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.java
@@ -1,43 +1,39 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        // AABABBA , k 1
-        // ABAB
-        // AAAABABBA , k = 1
-        // ABBBA , k = 1
-        // Naive Approach: 
-        // Set sliding window using two pointer, 
-        // pin the left and right pointer to move on, 
-        // in the sliding window, if smaller amount of char's occurance is bigger than k (euqivalent to
-        // we don't need to sustain this current sliding window with longest same str with replacement)
-        // we move left pointer to the right until that smaller char's occurence is not bigger than k. 
-        
-        
-        
-        // TC of this solution would be O(2N) -> O(N) (it iterates at most 2 times of length of String), where N is length of s
-        
-        //
         int l = 0;
         int r = 0;
-        int[] ch = new int[128];
-        int answer = 0;
-        
+        int[] charCounts = new int[26];
+        int answer = 1;
+        //  ^ -> requiredCount = windowLen - largestMaxCount
+        // Z(SA)AAB
         while(r < s.length()) {
-            ch[s.charAt(r)]++;
-            while(r - l + 1 - (getMaxOccurences(ch)) > k) {
-                ch[s.charAt(l)]--;
+            char currChar = s.charAt(r);
+            charCounts[currChar - 'A']++;
+            
+            while(getRequiredRepCount(charCounts, l, r) > k) {
+                char leftChar = s.charAt(l);
+                charCounts[leftChar -'A']--;
                 l++;
             }
+            
             answer = Math.max(r - l + 1, answer);
             r++;
         }
+        
+        
         return answer;
     }
     
-    private int getMaxOccurences(int[] charArr) {
-        int res = 0;
-        for(int occurence : charArr) {
-            res = Math.max(occurence, res);
+    
+    private int getRequiredRepCount(int[] charCounts, int start, int end) {
+        int totalCnts = 0;
+        int maxCharCount = 0;
+        
+        for(int count : charCounts) {
+            totalCnts += count;
+            maxCharCount = Math.max(count, maxCharCount);
         }
-        return res;
+        
+        return totalCnts - maxCharCount;
     }
 }
