233b778386cbe94f039812545dad8bd215b83e86
brwnsugr <eunho@tumblbug.com>
2022-08-24 09:27:49 +0900

    Time: 8 ms (80.57%), Space: 45 MB (51.58%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -1,28 +1,26 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        
-        
-        // abcab -> [2,1,1]
-        
+        // a(bca)bc,   a: 0, b: 1, c: 2, a: 3
+        // pw(wke)w , p:0, w:2, k:, if we reach out to the exsiting char, update latest pos to (prevpos, leftMost)
+        if(s.length() == 0) return 0;
+        if(s.length() == 1) return 1;
+        Map<Character, Integer> positionMap = new HashMap<>();
+        // for(char c : s.toCharArray()) countMap.put(c, countMap.getOrDefault(c, 0) + 1);
+        int l = -1;
         int r = 0;
-        int l = 0;
-        int[] count = new int[128];
         int answer = 0;
         
-        while( r < s.length()) {
-            char curr = s.charAt(r);
-            
-            count[curr]++;
-            
-            while(count[curr] > 1) {
-                char leftCh = s.charAt(l);
-                count[leftCh]--;
-                l++;
+        for(r = 0; r < s.length(); r++) {
+            char currChar = s.charAt(r);
+            if(!positionMap.containsKey(currChar)) {
+                positionMap.put(currChar, r);
             }
-            
-            answer = Math.max(answer, r - l + 1);
-            
-            r++;
+            else {
+                int prevPosition = positionMap.get(currChar);
+                l = Math.max(prevPosition, l);
+                positionMap.put(currChar, r);
+            }
+            answer = Math.max(r - l, answer);
         }
         return answer;
     }
