8f16b382c941fa72e065f89a6e6aaa5395a3cde0
brwnsugr <eunho@tumblbug.com>
2022-08-25 13:14:26 +0900

    Time: 6 ms (38.76%), Space: 48.2 MB (22.19%) - LeetHub

--- 261-graph-valid-tree/261-graph-valid-tree.java
+++ 261-graph-valid-tree/261-graph-valid-tree.java
@@ -1,39 +1,36 @@
 class Solution {
-    private List<List<Integer>> adjList = new ArrayList<>();
     
-    public boolean validTree(int n, int[][] edges) {    
+    private Set<Integer> visited = new HashSet<>();
+    
+    
+    public boolean validTree(int n, int[][] edges) {
+        List<List<Integer>> adjList = new ArrayList<>();
+        
         for(int i = 0; i < n; i++) adjList.add(new ArrayList<>());
+        
         for(int[] edge : edges) {
-            int prev = edge[0];
-            int dest = edge[1];
-            adjList.get(prev).add(dest);
-            adjList.get(dest).add(prev);
+            adjList.get(edge[0]).add(edge[1]);
+            adjList.get(edge[1]).add(edge[0]);
         }
-        Map<Integer, Integer> parentMap = new HashMap<>();
-        parentMap.put(0, -1);
-        return dfs(0, parentMap) && parentMap.size() == n;
+        boolean valid = isValid(adjList, 0, -1);
+        
+        return visited.size() != n ? false : valid;
     }
     
+    private boolean isValid(List<List<Integer>> adjList, int curr, int prev) {
+        if(visited.contains(curr)) {
+            return false;
+        }
     
-    private boolean dfs(int curr, Map<Integer, Integer> parentMap) {
-        for(int neighbor : adjList.get(curr)) {
-            if(parentMap.get(curr) == neighbor) continue;
-            if(parentMap.containsKey(neighbor)) return false;
-            parentMap.put(neighbor, curr);
-            boolean neighborACyclic = dfs(neighbor, parentMap);
-            // if(!neighborACyclic) return false;
+        visited.add(curr); 
+        List<Integer> neighbors = adjList.get(curr);
+        for(int neighbor : neighbors) {
+            if(neighbor == prev) continue;
+            if(!isValid(adjList, neighbor, curr)) return false;
+            
         }
         return true;
     }
+    
+    
 }
-
-/**
-
-        1(n)
-       2^ 3
-      4(n)
-
-map(2: 1, )
-
-
-**/
