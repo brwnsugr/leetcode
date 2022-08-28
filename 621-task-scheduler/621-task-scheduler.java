55ff5e517ed26f261691686a224c9c6cd964865c
brwnsugr <eunho@tumblbug.com>
2022-08-28 10:34:17 +0900

    Time: 42 ms (41.52%), Space: 72.2 MB (13.61%) - LeetHub

--- 621-task-scheduler/621-task-scheduler.java
+++ 621-task-scheduler/621-task-scheduler.java
@@ -1,40 +1,32 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-        // t = 0
-        // n = 2
-        // pq = (A: 4), B: 3, 
-        // q = A: 2, B: 3, 
-        // pq = (4, 3)
-        //
-        // pq = 3, 3 t = 3
-        // q = [(3,4), (2,5), ]
-        int timeElapsed = 0;
-        
+        int time = 0;
         PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b-a);
         Queue<Pair<Integer, Integer>> q = new LinkedList<>();
         
-        
         int[] counts = new int[26];
+        
         for(char task : tasks) counts[task - 'A']++;
         
         for(int count : counts) {
-            if(count > 0) pq.add(count);
+            if(count > 0 ) pq.add(count);
         }
         
         while(!pq.isEmpty() || !q.isEmpty()) {
-            timeElapsed++;
-            
             if(!pq.isEmpty()) {
-                int val = pq.poll();
-                val--;
-                if(val > 0) q.add(new Pair<Integer, Integer>(val, timeElapsed + n));
+                int topTask = pq.poll();
+                topTask--;
+                if(topTask > 0) {
+                    q.add(new Pair<Integer,Integer>(topTask, time + n));
+                }
             }
             
-            if(!q.isEmpty() && q.peek().getValue() == timeElapsed) {
+            if(!q.isEmpty() && q.peek().getValue() == time) {
                 pq.add(q.poll().getKey());
             }
+            time++;
         }
         
-        return timeElapsed;
+        return time;
     }
 }
