285ba9063e07b516a499a76bd84e94251f242bce
brwnsugr <eunho@tumblbug.com>
2022-08-28 10:03:19 +0900

    Time: 3 ms (53.72%), Space: 58.3 MB (17.75%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.java
+++ 238-product-of-array-except-self/238-product-of-array-except-self.java
@@ -1,15 +1,27 @@
 class Solution {
-  public int[] productExceptSelf(int[] nums) {
-    int n = nums.length;
-    int[] answer = new int[n];
-    answer[0] = 1;
-    for(int i = 1; i < n; i++) answer[i] = nums[i-1] * answer[i-1];
-
-    int R = 1;
-    for(int i = n-1; i>=0; i--) {
-      answer[i] *= R;
-      R *= nums[i];
+    public int[] productExceptSelf(int[] nums) {
+        int len = nums.length;
+        int[] leftProducts = new int[len];
+        int[] rightProducts = new int[len];
+        int leftProduct = 1;
+        int rightProduct = 1;
+        for(int i = 0; i < len; i++) {
+            leftProduct = leftProduct * nums[i];
+            leftProducts[i] = leftProduct;
+            rightProduct = rightProduct * nums[len - i - 1];
+            rightProducts[len - i - 1] = rightProduct;
+        }
+        
+        int[] ans = new int[len];
+        
+        for(int i = 0; i < len; i++) {
+            if(i == 0) ans[i] = rightProducts[1];
+            else if(i == len -1) ans[i] = leftProducts[len - 2];
+            else {
+                ans[i] = leftProducts[i - 1] * rightProducts[i + 1];
+            }
+        }
+        
+        return ans;
     }
-    return answer;
-  }
 }
