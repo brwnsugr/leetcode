6c966113d716834e13b936edfeaa95d06a640fd3
brwnsugr <eunho@tumblbug.com>
2022-09-10 19:43:40 +0900

    Time: 2 ms (74.53%), Space: 72.7 MB (83.25%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -1,13 +1,13 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int tempSum = 0;
-        int answer = Integer.MIN_VALUE;
-        for(int num : nums) {
-            tempSum += num;
-            answer = Math.max(answer, tempSum);
-            if(tempSum < 0) tempSum = 0;
-        }
+        int answer = nums[0];
+        int currentSum = nums[0];
         
+        for(int i = 1; i < nums.length; i++) {
+            int num = nums[i];
+            currentSum = Math.max(currentSum + num, num);
+            answer = Math.max(currentSum, answer);
+        }
         return answer;
     }
 }
