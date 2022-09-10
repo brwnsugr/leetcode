1d3eb2ca2e57b9fe9405792fe53c7501ea097d86
brwnsugr <eunho@tumblbug.com>
2022-09-10 22:34:02 +0900

    Time: 2 ms (95.45%), Space: 62.6 MB (89.35%) - LeetHub

--- 134-gas-station/134-gas-station.java
+++ 134-gas-station/134-gas-station.java
@@ -1,24 +1,18 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        int totalGas = 0; 
-        int totalCost = 0;
         int len = gas.length;
-        for(int i = 0; i < len; i++) {
-            totalGas += gas[i];
-            totalCost += cost[i];
-        }
-        
-        if(totalCost > totalGas) return -1;
-        
+
+        int totalBalance = 0;
         int balance = 0;
         int res = 0;
         for(int i = 0; i < len; i++) {
             balance += gas[i] - cost[i];
+            totalBalance += gas[i] - cost[i];
             if(balance < 0) {
                 res = i + 1;
                 balance = 0;
             }
         }
-        return res;
+        return totalBalance >= 0 ? res : -1;
     }
 }
